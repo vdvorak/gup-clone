6b2cbcca2c86d4131d545375743fce4d78da7118
Sasha Minchuck <minchuck.sasha@gmail.com>
2015-12-22 18:15:54 +0200

    unauthorized page; unauthorized ex handler;

--- src/main/java/ua/com/itproekt/gup/util/SecurityOperations.java
+++ src/main/java/ua/com/itproekt/gup/util/SecurityOperations.java
@@ -1,9 +1,12 @@
 package ua.com.itproekt.gup.util;
 
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import ua.com.itproekt.gup.model.login.LoggedUser;
+
+
 
 public final class SecurityOperations {
 
@@ -13,12 +16,18 @@ public final class SecurityOperations {
     }
 
     public static String getLoggedUserId() {
-        return ((LoggedUser)SecurityContextHolder.getContext().getAuthentication()
-                .getPrincipal()).getProfileId();
+        return getLoggedUser().getProfileId();
     }
 
     public static String getLoggedUserEmail() {
-        return ((LoggedUser)SecurityContextHolder.getContext().getAuthentication()
-                .getPrincipal()).getUsername();
+        return getLoggedUser().getUsername();
+    }
+
+    public static LoggedUser getLoggedUser() {
+        LoggedUser loggedUser = (LoggedUser)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (loggedUser == null) {
+            throw new AccessDeniedException("You don't have the appropriate privileges to access this resource");
+        }
+        return loggedUser;
     }
 }
