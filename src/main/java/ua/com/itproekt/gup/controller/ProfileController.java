6ceca8d2bb42608e9c3c80be93909f97010e8968
Alexander <alexdubitandum@gmail.com>
2016-01-05 15:18:51 +0200

    optimize import

--- src/main/java/ua/com/itproekt/gup/controller/ProfileController.java
+++ src/main/java/ua/com/itproekt/gup/controller/ProfileController.java
@@ -1,30 +1,14 @@
 package ua.com.itproekt.gup.controller;
 
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ua.com.itproekt.gup.model.profiles.Profile;
-import ua.com.itproekt.gup.model.profiles.UserRole;
 import ua.com.itproekt.gup.service.activityfeed.ActivityFeedService;
 import ua.com.itproekt.gup.service.profile.ProfilesService;
 import ua.com.itproekt.gup.util.SecurityOperations;
-
-import java.net.URI;
-import java.util.HashSet;
 
 /**
  * Created by Optical Illusion on 17.11.2015.
@@ -87,5 +71,4 @@ public class ProfileController {
         model.addAttribute("profile", profile);
         return "edit-profile";
     }
-
 }
