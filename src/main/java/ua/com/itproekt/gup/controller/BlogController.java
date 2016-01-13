8729ce215c97803f8d068a6464c5b151861be338
Alexander <alexdubitandum@gmail.com>
2016-01-13 21:02:14 +0200

    blog-edit page initial + BlogController update

--- src/main/java/ua/com/itproekt/gup/controller/BlogController.java
+++ src/main/java/ua/com/itproekt/gup/controller/BlogController.java
@@ -1,8 +1,14 @@
 package ua.com.itproekt.gup.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ua.com.itproekt.gup.model.news.Blog;
+import ua.com.itproekt.gup.service.news.BlogService;
+import ua.com.itproekt.gup.util.SecurityOperations;
 
 /**
  * Created by Optical Illusion on 13.01.2016.
@@ -10,8 +16,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BlogController {
 
+    @Autowired
+    BlogService blogService;
+
     @RequestMapping("/blog-create")
     public String getBlogCreatePage() {
         return "blog-create";
+    }
+
+    @RequestMapping(value = "/blog/{id}/edit", method = RequestMethod.POST)
+    public String getBlogEditPage(Model model, @PathVariable String id) {
+
+        String userId = SecurityOperations.getLoggedUserId();
+
+        Blog oldBlog = blogService.findBlog(id);
+
+        if (oldBlog.getAuthorId().equals(userId)) {
+            model.addAttribute("profile", oldBlog);
+            return "blog-edit";
+        } else {
+            return "index";
+        }
     }
 }
---
+++ src/main/webapp/WEB-INF/views/blog-edit.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Optical Illusion
+  Date: 13.01.2016
+  Time: 20:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Редактро блога</title>
+</head>
+<body>
+
+</body>
+</html>
