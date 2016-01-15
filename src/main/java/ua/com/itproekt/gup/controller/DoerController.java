d03ec8414ea6fea7da293ff9b6a57ca2a668313d
YuliiaMarchuk <fairy.freyja@gmail.com>
2016-01-15 19:04:22 +0200

    add jsp and Controller for view Doer

--- src/main/java/ua/com/itproekt/gup/controller/DoerController.java
+++ src/main/java/ua/com/itproekt/gup/controller/DoerController.java
@@ -1,15 +1,51 @@
 package ua.com.itproekt.gup.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ua.com.itproekt.gup.model.profiles.Profile;
+import ua.com.itproekt.gup.model.tender.doer.Doer;
+import ua.com.itproekt.gup.service.profile.ProfilesService;
+import ua.com.itproekt.gup.service.tender.doer.DoerService;
+import ua.com.itproekt.gup.util.SecurityOperations;
 
 /**
  * Created by Fairy on 15.01.2016.
  */
 @Controller
 public class DoerController {
+    @Autowired
+    DoerService doerService;
+
+    @Autowired
+    ProfilesService profilesService;
+
     @RequestMapping("/doer-create")
-    public String getBlogCreatePage() {
+    public String getDoerCreatePage() {
         return "doer-create";
+    }
+
+    @RequestMapping(value = "/doer/{id}", method = RequestMethod.GET)
+    public String getDoerRead(Model model, @PathVariable String id) {
+        boolean check = false;
+
+        Doer doer = doerService.findById(id);
+
+        if (SecurityOperations.isUserLoggedIn()) {
+            String userId = SecurityOperations.getLoggedUserId();
+            check = userId.equals(doer.getAuthorId());
+            model.addAttribute("check", check);
+        }
+
+        Profile profile = profilesService.findById(doer.getAuthorId());
+        String userName = profile.getUsername();
+
+        model.addAttribute("username", userName);
+        model.addAttribute("check", check);
+        model.addAttribute("doer", doer);
+        return "doer";
     }
 }
---
+++ src/main/webapp/WEB-INF/views/doer.jsp
@@ -0,0 +1,80 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>${doer.title} | Портал GUP</title>
+</head>
+<body>
+<div>
+  ${doer.title}
+</div>
+КВЭД <span id="rubrics"></span>
+<div>
+
+</div>
+<div>
+  <c:choose>
+    <c:when test="${not empty doer.imageId}">
+      <img id="imgPreview" src="/api/rest/fileStorage/NEWS/file/read/id/${doer.imageId}" width="200"
+           height="200">
+    </c:when>
+    <c:otherwise>
+      <img id="imgPreview" src="/resources/images/no_photo.jpg" width="200" height="200">
+    </c:otherwise>
+  </c:choose>
+</div>
+<div>
+  Название
+  <br>
+  ${doer.authorId}
+</div>
+
+<div>
+  Автор: ${username}
+</div>
+<div>
+  Описание
+  <br>
+  ${doer.body}
+</div>
+<div>
+  Количество посещений
+  <br>
+  ${doer.countVisit}
+</div>
+<div>
+  Дата создания
+  <br>
+  ${doer.dateOfCreate}
+</div>
+<div>
+  Дата последнего обноновления
+  <br>
+  ${doer.dateOfUpdate}
+</div>
+
+<div>
+  Дата последнего обноновления
+  <br>
+  ${doer.dateOfUpdate}
+</div>
+
+<c:if test="${check}">
+  <a href="/doer/update/${doer.id}"><button>Редактировать</button></a>
+
+</c:if>
+
+<script src="/resources/libs/jquery-1.11.3.min.js"></script>
+<script src="/resources/libs/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+
+<script>
+  var categories = '${doer.naceIds}'.replace('[', '').replace(']', '').replace(' ', '').split(','); // make array from string
+
+  for (var i = 0; i < categories.length; i++) {
+    var rubric = $('#rubrics');
+        rubric.append(categories[i]);
+  }
+</script>
+</body>
+</html>
