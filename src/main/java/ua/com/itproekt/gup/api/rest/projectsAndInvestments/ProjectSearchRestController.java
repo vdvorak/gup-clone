5c64b55db45b6d5b4f89d19359f134845faf4d98
Sasha Minchuck <minchuck.sasha@gmail.com>
2016-01-22 15:33:02 +0200

    autocomplete search input

--- src/main/java/ua/com/itproekt/gup/api/rest/projectsAndInvestments/ProjectSearchRestController.java
+++ src/main/java/ua/com/itproekt/gup/api/rest/projectsAndInvestments/ProjectSearchRestController.java
@@ -1,21 +1,21 @@
-package ua.com.itproekt.gup.api.rest.news;
+package ua.com.itproekt.gup.api.rest.projectsAndInvestments;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ua.com.itproekt.gup.service.news.BlogPostService;
+import ua.com.itproekt.gup.service.projectsAndInvestments.project.ProjectService;
 
 import java.util.Set;
 
 @RestController
-public class BlogPostSearch {
+public class ProjectSearchRestController {
 
     @Autowired
-    BlogPostService blogPostService;
+    ProjectService projectService;
 
-    @RequestMapping("/search/blogPost")
+    @RequestMapping("/search/project")
     public Set<String> getMachedNames(@RequestParam String term){
-        return blogPostService.getMatchedNames(term);
+        return projectService.getMatchedNames(term);
     }
 }
--- src/main/webapp/WEB-INF/templates/main-search-bar.jsp
+++ src/main/webapp/WEB-INF/templates/main-search-bar.jsp
@@ -8,7 +8,21 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <section class="first-sec">
   <div class="main-search-button-wrapper">
-    <input type="text" placeholder="Поиск">
+    <input id="tagsName" type="text" placeholder="Введите имя пользователя или компании">
     <a href="">Найти<span class="main-search-button-icon"><img src="/resources/img/magnifire.png"></span></a>
   </div>
+  <%--<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">--%>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+  <script>
+    $(function() {
+      $("#tagsName").autocomplete({
+        source: function (request, response) {
+          $.getJSON("${pageContext.request.contextPath}/search/profile", {
+            term: request.term
+          }, response);
+        }
+      });
+    });
+  </script>
 </section>
