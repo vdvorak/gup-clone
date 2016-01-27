5286be1fa1eeedb04a255c37753c3045aa695dd3
YuliiaMarchuk <fairy.freyja@gmail.com>
2016-01-27 18:16:09 +0200

    add default password to admin dialogue support

--- src/main/java/ua/com/itproekt/gup/util/StaticData.java
+++ src/main/java/ua/com/itproekt/gup/util/StaticData.java
@@ -16,9 +16,12 @@ public final class StaticData {
     public static final String SUPPORT_EMAIL = "inform@e-otg.com";
     public static final String SUPPORT_EMAIL_PASSWORD = "Kt801Ma5cYuI";
     public static final String SUPPORT_ADMIN_USERNAME = "Специалист службы поддержки";
+    public static final String SUPPORT_ADMIN_PASSWORD = "admin";
+
     static {
         SUPPORT_DIALOGUE_ADMIN.setId(SUPPORT_DIALOGUES_ADMIN_ID);
         SUPPORT_DIALOGUE_ADMIN.setUsername(SUPPORT_ADMIN_USERNAME);
+        SUPPORT_DIALOGUE_ADMIN.setPassword(SUPPORT_ADMIN_PASSWORD);
         SUPPORT_DIALOGUE_ADMIN.setEmail(SUPPORT_EMAIL);
         Set<UserRole> roles = new HashSet<>();
         roles.add(UserRole.ROLE_ADMIN);
