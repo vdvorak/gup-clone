53b24f057f5ac1498699be296e17c5cd3981718c
Alexander <alexdubitandum@gmail.com>
2016-01-28 11:17:25 +0200

    tender.jsp init

--- src/main/java/ua/com/itproekt/gup/controller/TenderController.java
+++ src/main/java/ua/com/itproekt/gup/controller/TenderController.java
@@ -1,20 +1,13 @@
 package ua.com.itproekt.gup.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ua.com.itproekt.gup.model.profiles.Profile;
-import ua.com.itproekt.gup.model.tender.Tender;
-import ua.com.itproekt.gup.model.tender.TenderFilterOptions;
 import ua.com.itproekt.gup.service.filestorage.StorageService;
 import ua.com.itproekt.gup.service.nace.NaceService;
 import ua.com.itproekt.gup.service.profile.ProfilesService;
 import ua.com.itproekt.gup.service.tender.TenderService;
-import ua.com.itproekt.gup.util.EntityPage;
-import ua.com.itproekt.gup.util.SecurityOperations;
 
 /**
  * Created by qz on 1/12/2016.
@@ -35,9 +28,14 @@ public class TenderController {
     StorageService storageService;
 
 
+    @RequestMapping("/tenders")
+    public String getAllTenders(Model model) {
+        return "tenders";
+    }
+
     @RequestMapping("/tender")
     public String getTender(Model model) {
-        return "tender";
+        return "tenders";
     }
 
     @RequestMapping("/tender-make")
