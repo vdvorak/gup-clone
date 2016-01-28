f8e5e4cd428ed4f8597ab56094d9dd4b85a57f6a
YuliiaMarchuk <fairy.freyja@gmail.com>
2016-01-28 14:52:33 +0200

    add new methods to DialogueRepository for support dialogues

--- src/main/java/ua/com/itproekt/gup/dao/dialogue/DialogueRepository.java
+++ src/main/java/ua/com/itproekt/gup/dao/dialogue/DialogueRepository.java
@@ -2,6 +2,7 @@ package ua.com.itproekt.gup.dao.dialogue;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ua.com.itproekt.gup.model.privatemessages.Dialogue;
 import ua.com.itproekt.gup.model.privatemessages.Member;
@@ -11,6 +12,8 @@ import java.util.List;
 public interface DialogueRepository  extends MongoRepository<Dialogue, String> {
     List<Dialogue> findByMembers(List<Member> members);
     List<Dialogue> findByMembersIn(Member member);
+    List<Dialogue> findByMembersIn(Member member, Sort sort);
+    List<Dialogue> findByMembersIn(Member member, Pageable pageable);
     Dialogue findByMembersAndSubject(List<Member> members, String subject);
 
 //    EntityPage<Dialogue> findWihOptions(DialogueFilterOption dialogueFilterOption);
