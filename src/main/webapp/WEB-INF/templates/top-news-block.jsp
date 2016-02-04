13f5497110c213c8cfaa809ade8e0de898318290
Sasha Minchuck <minchuck.sasha@gmail.com>
2016-02-04 17:36:23 +0200

    add js function for show next newsBtn

--- src/main/webapp/WEB-INF/templates/top-news-block.jsp
+++ src/main/webapp/WEB-INF/templates/top-news-block.jsp
@@ -17,44 +17,44 @@
     </div>
 
     <div id="topUserNews">
-        <div class="tidi4">
-            <p class="data">Опубликовано: 22.10.16</p>
-            <a href="#"><img src="/resources/images/tidi4.png" alt="tidi"></a>
-            <a class="tidi4-a" href="#">MONARCHY</a>
-            <p class="tidi4-p">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>
-        </div>
-        <div class="tidi4">
-            <p class="data-tidi5">Опубликовано: 22.10.16</p>
-            <a class="tidi4-ar" href="#">MONARCHY</a>
-            <a class="tidi5-img" href="#"><img src="/resources/images/tidi5.png" alt="tidi"></a>
-            <p class="tidi4-pr">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>
-        </div>
+        <%--<div class="tidi4">--%>
+            <%--<p class="data">Опубликовано: 22.10.16</p>--%>
+            <%--<a href="#"><img src="/resources/images/tidi4.png" alt="tidi"></a>--%>
+            <%--<a class="tidi4-a" href="#">MONARCHY</a>--%>
+            <%--<p class="tidi4-p">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>--%>
+        <%--</div>--%>
+        <%--<div class="tidi4">--%>
+            <%--<p class="data-tidi5">Опубликовано: 22.10.16</p>--%>
+            <%--<a class="tidi4-ar" href="#">MONARCHY</a>--%>
+            <%--<a class="tidi5-img" href="#"><img src="/resources/images/tidi5.png" alt="tidi"></a>--%>
+            <%--<p class="tidi4-pr">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>--%>
+        <%--</div>--%>
 
-        <div class="tidi4">
-            <p class="data">Опубликовано: 22.10.16</p>
-            <a href="#"><img src="/resources/images/tidi6.png" alt="tidi"></a>
-            <a class="tidi4-a" href="#">MONARCHY</a>
-            <p class="tidi4-p">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>
-        </div>
-        <div class="tidi4">
-            <p class="data-tidi5">Опубликовано: 22.10.16</p>
-            <a class="tidi4-ar" href="#">MONARCHY</a>
-            <a class="tidi5-img" href="#"><img src="/resources/images/tidi7.png" alt="tidi"></a>
-            <p class="tidi4-pr">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>
-        </div>
+        <%--<div class="tidi4">--%>
+            <%--<p class="data">Опубликовано: 22.10.16</p>--%>
+            <%--<a href="#"><img src="/resources/images/tidi6.png" alt="tidi"></a>--%>
+            <%--<a class="tidi4-a" href="#">MONARCHY</a>--%>
+            <%--<p class="tidi4-p">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>--%>
+        <%--</div>--%>
+        <%--<div class="tidi4">--%>
+            <%--<p class="data-tidi5">Опубликовано: 22.10.16</p>--%>
+            <%--<a class="tidi4-ar" href="#">MONARCHY</a>--%>
+            <%--<a class="tidi5-img" href="#"><img src="/resources/images/tidi7.png" alt="tidi"></a>--%>
+            <%--<p class="tidi4-pr">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>--%>
+        <%--</div>--%>
     </div>
 
     <div class="clearfix"></div>
 
-    <div id="nextUserNews">
-        <div class="tidi-drop">
-            <div class="tidi4">
-                <p class="data">Опубликовано: 22.10.16</p>
-                <a href="#"><img src="/resources/images/tidi4.png" alt="tidi"></a>
-                <a class="tidi4-a" href="#">MONARCHY</a>
-                <p class="tidi4-p">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>
-            </div>
-        </div>
+    <div id="nextUserNews" class="tidi-drop" >
+        <%--<div class="tidi-drop" >--%>
+            <%--&lt;%&ndash;<div class="tidi4">&ndash;%&gt;--%>
+                <%--&lt;%&ndash;<p class="data">Опубликовано: 22.10.16</p>&ndash;%&gt;--%>
+                <%--&lt;%&ndash;<a href="#"><img src="/resources/images/tidi4.png" alt="tidi"></a>&ndash;%&gt;--%>
+                <%--&lt;%&ndash;<a class="tidi4-a" href="#">MONARCHY</a>&ndash;%&gt;--%>
+                <%--&lt;%&ndash;<p class="tidi4-p">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>&ndash;%&gt;--%>
+            <%--&lt;%&ndash;</div>&ndash;%&gt;--%>
+        <%--</div>--%>
     </div>
 
 
--- src/main/webapp/resources/js/top-news-block.js
+++ src/main/webapp/resources/js/top-news-block.js
@@ -1,71 +1,136 @@
 var blogPostFO = {};
 blogPostFO.createdDateSortDirection = "DESC";
 blogPostFO.skip = 0;
-blogPostFO.limit = 10;
+blogPostFO.limit = 5;
 
-loadTopNews(blogPostFO);
+loadAndAppendTopUserNews(blogPostFO);
+loadAndAppendNextUserNews(blogPostFO);
+loadAndAppendNextUserNews(blogPostFO);
+loadAndAppendNextUserNews(blogPostFO);
 
-function loadTopNews(blogPostFO) {
-    $.ajax({
+
+function getNewsResponse(blogPostFO) {
+    return $.ajax({
         type: "POST",
         contentType: "application/json; charset=utf-8",
         url: "/api/rest/newsService/blogPost/read/all",
-        data: JSON.stringify(blogPostFO),
-        success: function (response) {
-            $(document).ready(function () {
-                var blogPosts = response.entities;
-                for (var i = 0; i < blogPosts.length; i++) {
-                    var blogPostURL = '/blog-post/view/' + blogPosts[i].id;
+        data: JSON.stringify(blogPostFO)
+    })
+}
 
-                    var createdDate = new Date(blogPosts[i].createdDate);
-                    blogPosts[i].createdDate = createdDate.getDate() + '.' + (createdDate.getMonth() + 1) + '.' + createdDate.getFullYear();
+function getBlogPostUrl(blogPost) {
+    return '/blog-post/view/' + blogPost.id;
+}
 
-                    var  imagePreviewTag = '';
-                    if (blogPosts[i].imagesIds !== null) {
-                        for (var key in blogPosts[i].imagesIds) {
-                            if (blogPosts[i].imagesIds[key] === "1") {
-                                imagePreviewTag = '<img src="/api/rest/fileStorage/NEWS/file/read/id/' + key + '" alt="tidi"  width="169" height="126">';
-                            }
-                        }
-                    } else {
-                        imagePreviewTag = '<img src="/resources/images/no_photo.jpg" alt="tidi" width="169" height="126">';
-                    }
+function getImagePreviewTag(blogPost) {
+    if (blogPost.imagesIds !== null) {
+        for (var key in blogPost.imagesIds) {
+            if (blogPost.imagesIds[key] === "1") {
+                return '<img src="/api/rest/fileStorage/NEWS/file/read/id/' + key + '" alt="tidi"  width="169" height="126">';
+            }
+        }
+    } else {
+        return '<img src="/resources/images/no_photo.jpg" alt="tidi" width="169" height="126">';
+    }
+}
 
-                    if (i % 2 === 0) {
-                        $('#nextUserNews').append(
-                            '<div class="tidi-drop">' +
-                                '<div class="tidi4">' +
-                                    '<p class="data">' +
-                                        'Опубликовано: ' + blogPosts[i].createdDate +
-                                    '</p>' +
-                                    '<a href="' + blogPostURL +'">' +
-                                        imagePreviewTag +
-                                    '</a>' +
-                                    '<a class="tidi4-a" href="' + blogPostURL +'">' +
-                                        blogPosts[i].title +
-                                    '</a>' +
-                                    '<p class="tidi4-p">' +
-                                        blogPosts[i].text.substring(0, 136) +
-                                        '<a class="tidi4-a2" href="' + blogPostURL +'">' +
-                                            'Продолжение ->' +
-                                        '</a>' +
-                                    '</p>' +
-                                '</div>' +
-                            '</div>');
-                    } else {
+function getReadableDate(blogPost) {
+    var createdDate = new Date(blogPost.createdDate);
+    return createdDate.getDate() + '.' + (createdDate.getMonth() + 1) + '.' + createdDate.getFullYear();
+}
 
-                    }
-                }
-            });
+function appendNewsWithLeftImg(elementId, blogPostURL, createdDate, imagePreviewTag, title, text) {
+    $('#' + elementId).append(
+        '<div class="tidi4">' +
+        '<p class="data">' +
+        'Опубликовано: ' + createdDate +
+        '</p>' +
+        '<a href="' + blogPostURL +'">' +
+        imagePreviewTag +
+        '</a>' +
+        '<a class="tidi4-a" href="' + blogPostURL +'">' +
+        title +
+        '</a>' +
+        '<p class="tidi4-p">' +
+        text.substring(0, 136) +
+        '<a class="tidi4-a2" href="' + blogPostURL +'">' +
+        'Продолжение ->' +
+        '</a>' +
+        '</p>' +
+        '</div>');
+}
+
+function appendNewsWithRightImg(elementId, blogPostURL, createdDate, imagePreviewTag, title, text) {
+    $('#' + elementId).append(
+        '<div class="tidi4">' +
+        '<p class="data-tidi5">' +
+        'Опубликовано: ' + createdDate +
+        '</p>' +
+        '<a class="tidi5-img" href="' + blogPostURL +'">' +
+        imagePreviewTag +
+        '</a>' +
+        '<a class="tidi4-ar" href="' + blogPostURL +'">' +
+        title +
+        '</a>' +
+        '<p class="tidi4-pr">' +
+        text.substring(0, 136) +
+        '<a class="tidi4-a2" href="' + blogPostURL +'">' +
+        'Продолжение ->' +
+        '</a>' +
+        '</p>' +
+        '</div>');
+}
+
+function loadAndAppendTopUserNews() {
+    var promise = getNewsResponse(blogPostFO);
+
+    promise.success(function (data) {
+        var blogPosts = data.entities;
+
+        for (var i = 0; i < blogPosts.length; i++) {
+            var blogPostURL = getBlogPostUrl(blogPosts[i]);
+            var createdDate = getReadableDate(blogPosts[i]);
+            var imagePreviewTag = getImagePreviewTag(blogPosts[i]);
+
+            if (i % 2 === 0) {
+                appendNewsWithLeftImg('topUserNews', blogPostURL, createdDate, imagePreviewTag, blogPosts[i].title, blogPosts[i].text);
+            } else {
+                appendNewsWithRightImg('topUserNews', blogPostURL, createdDate, imagePreviewTag, blogPosts[i].title, blogPosts[i].text);
+            }
         }
     });
 }
 
+function loadAndAppendNextUserNews() {
 
+    if (blogPostFO.skip == 0) {
+        blogPostFO.skip = blogPostFO.limit;
+        blogPostFO.limit = 1;
+    } else {
+        blogPostFO.skip += 1;
+    }
 
-//    <div class="tidi4">
-//    <p class="data-tidi5">Опубликовано: 22.10.16</p>
-//<a class="tidi4-ar" href="#">MONARCHY</a>
-//    <a class="tidi5-img" href="#"><img src="/resources/images/tidi5.png" alt="tidi"></a>
-//    <p class="tidi4-pr">Monarchy — английский музыкальный электронный дуэт из Лондона, состоящий из Эндрю Армстронга и Ра Блэка. Ранее были известны как Milke.. <a class="tidi4-a2" href="#">Продолжение -></a></p>
-//    </div>
+    var promise = getNewsResponse(blogPostFO);
+
+    promise.success(function (data) {
+        var blogPosts = data.entities;
+
+        for (var i = 0; i < blogPosts.length; i++) {
+            var blogPostURL = getBlogPostUrl(blogPosts[i]);
+            var createdDate = getReadableDate(blogPosts[i]);
+            var imagePreviewTag = getImagePreviewTag(blogPosts[i]);
+
+            if (i % 2 === 0) {
+                appendNewsWithLeftImg('nextUserNews', blogPostURL, createdDate, imagePreviewTag, blogPosts[i].title, blogPosts[i].text);
+            } else {
+                appendNewsWithRightImg('nextUserNews', blogPostURL, createdDate, imagePreviewTag, blogPosts[i].title, blogPosts[i].text);
+            }
+        }
+    });
+}
+
+function loadAndAppendAdNews() {
+    //oskdskdkkk
+    //oskdskdkkk
+    //oskdskdkkk
+}
