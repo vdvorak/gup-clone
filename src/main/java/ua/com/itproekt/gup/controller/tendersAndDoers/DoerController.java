7c5b067b49e0a18c305241efa6694cbb345fc436
YuliiaMarchuk <fairy.freyja@gmail.com>
2016-02-27 15:23:10 +0200

    update DoerController

--- src/main/java/ua/com/itproekt/gup/controller/tendersAndDoers/DoerController.java
+++ src/main/java/ua/com/itproekt/gup/controller/tendersAndDoers/DoerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import ua.com.itproekt.gup.model.profiles.Profile;
 import ua.com.itproekt.gup.model.tender.doer.Doer;
 import ua.com.itproekt.gup.model.tender.doer.DoerFilterOptions;
+import ua.com.itproekt.gup.model.tender.doer.Recall;
 import ua.com.itproekt.gup.service.profile.ProfilesService;
 import ua.com.itproekt.gup.service.tender.doer.DoerService;
 import ua.com.itproekt.gup.util.EntityPage;
@@ -77,6 +78,8 @@ public class DoerController {
         else model.addAttribute("username", "Anonimous");
         model.addAttribute("check", check);
         model.addAttribute("doer", doer);
+        model.addAttribute("like", doer.getRecalls().stream().filter(r -> r.getMark().equals(Recall.Mark.LIKE)));
+        model.addAttribute("dislike", doer.getRecalls().stream().filter(r -> r.getMark().equals(Recall.Mark.DISLIKE)));
         return "tendersAndDoers/doers/doer";
     }
 }
