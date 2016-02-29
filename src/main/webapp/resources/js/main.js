0770bb7d6ad6402c2f8b786ec7bd8ed197bae65e
Sasha Minchuck <minchuck.sasha@gmail.com>
2016-02-29 13:25:57 +0200

    fix commit main.js

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,3 +1,18 @@
+function getUrlParam(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? null : sParameterName[1];
+        }
+    }
+}
+
 $(document).ready(function(){
 	$("#ad-caret").click(function(){
         $('.add-top1:first').clone().insertAfter('.add-top1:last');
