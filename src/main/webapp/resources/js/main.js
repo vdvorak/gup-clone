6af00f171a08ca9fb22d16317c944822be993561
Alexander <alexdubitandum@gmail.com>
2016-02-29 15:26:59 +0200

    main.js fix

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -252,4 +252,69 @@ $(document).ready(function(){
         slideWidth: 140,
         slideMargin: 20
     });
+
+    //
+
+
+    //
+
+    $('#header_money_amount').on('input', function () {
+        $.ajax({
+            url: '/account/getLiqPayParam',
+            method: 'POST',
+            data: {'amount': $('#money_amount').val()},
+            success: function (response) {
+                $('#liq-pay-data').val(response[0]);
+                $('#liq-pay-signature').val(response[1]);
+            }
+        });
+    });
+
+    $('#modal_money_amount').on('input', function () {
+        $.ajax({
+            url: '/account/getLiqPayParam',
+            method: 'POST',
+            data: {'amount': $('#modal_money_amount').val()},
+            success: function (response) {
+                $('#modal_liq-pay-data').val(response[0]);
+                $('#modal_liq-pay-signature').val(response[1]);
+            }
+        });
+    });
+
+    $('.modal-pay-liq-pay').on('click', function () {
+        $('#modal-bill-submit').click()
+
+    })
+
+    $.ajax({
+        type: "POST",
+        url: "/check-balance",
+        cache: false,
+        success: function (response) {
+            $('#score').text(response);
+
+            if (response >= 50) {
+                $('.brokeAss').hide();
+                $('.richAss').show()
+            } else {
+                $('.brokeAss').show();
+                $('.richAss').hide()
+            }
+        }
+    });
+
+
+    $('#noMoneyStartRich').on('click', function () {
+        $.ajax({
+            type: "POST",
+            url: "/api/rest/profilesService/join-organization",
+            cache: false,
+            success: function (response) {
+                if (response == "2") {
+                    alert("Поздравляем со вступлением в организацию!")
+                }
+            }
+        });
+    })
 });
