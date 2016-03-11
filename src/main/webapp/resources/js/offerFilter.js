b884af94ad1628c1db0b3d0c42cc6e3c7288e120
Julia <Julia>
2016-03-11 15:20:06 +0200

    Offer filter class

---
+++ src/main/webapp/resources/js/offerFilter.js
@@ -0,0 +1,126 @@
+var OfferFilter = (function($) {
+
+    'use strict';
+
+    function OfferFilter() {
+        this.skip = 0;
+        this.limit = 10;
+    }
+
+    OfferFilter.prototype.cleanResult = function () {
+        var offerBoxArr = $('ul.notice-box:not(:first)');
+        for (var i = 0; i < offerBoxArr.length; i++) {
+            offerBoxArr[i].remove();
+        }
+        $('ul.notice-box:first').text("");
+
+        return this;
+    }
+
+    OfferFilter.prototype.readAllByFilter = function () {
+
+        $.ajax({
+            type: "POST",
+            url: "/api/rest/offersService/offer/read/all",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify(this),
+            statusCode: {
+                200: function (data, textStatus, request) {
+                    drawOffers(data.entities);
+                }
+            }
+        });
+        return this;
+    }
+
+    function createNewBox() {
+        var ulLast = $('ul.notice-box:last');
+        ulLast.clone()
+            .text("")
+            .insertAfter(ulLast);
+    }
+
+    function getSrcOfMainImg(imagesIds) {
+        var imgSrc = "";
+        var arrKeys = Object.keys(imagesIds);
+        if (arrKeys.length) {
+            for (var key in imagesIds) {
+                if (imagesIds[key] === 'pic1') {
+                    imgSrc = '/api/rest/fileStorage/OFFERS/file/read/id/' + key;
+                    break;
+                }
+            }
+            if (imgSrc === '') imgSrc = '/api/rest/fileStorage/OFFERS/file/read/id/' + arrKeys[0];
+        } else {
+            imgSrc = "/resources/images/no_photo.jpg";
+        }
+
+        return imgSrc;
+    }
+
+    function getPriceStr(price, currency) {
+        var priceStr = "Нет цены";
+        if (price) {
+            priceStr = price.toString();
+            if (currency) {
+                priceStr = priceStr + currency;
+            }
+        }
+        return priceStr;
+    }
+
+    function createNewOffer(offerObj) {
+
+        var imgSrc = getSrcOfMainImg(offerObj.imagesIds);
+        var priceStr = getPriceStr(offerObj.price, offerObj.currency);
+
+        var newLi = $('#li-offer-basic').clone()
+            .attr('id', "")
+            .css("display", "inline-block");
+        newLi.find('p').text(offerObj.title);
+        newLi.find('.image').attr("href", '/offer/' + offerObj.id + '');
+        newLi.find('img').attr("src", imgSrc);
+
+        newLi.children('span').text("Просмотров: " + offerObj.views);
+        newLi.find('a.btn').text(priceStr).attr("href", '/offer/' + offerObj.id + '');
+
+        return newLi;
+    }
+
+    function drawOffers(offersArr) {
+
+        var count = 0;
+        var maxCount = 5;
+
+        for (var i = 0; i < offersArr.length; i++) {
+            var newOffer = createNewOffer(offersArr[i]);
+
+            if (count === maxCount) {
+                count = 0;
+                createNewBox();
+            }
+            newOffer.appendTo($('ul.notice-box:last'));
+            count++;
+        }
+    }
+
+    OfferFilter.prototype.setFilterProperties = function () {
+        return this;
+    }
+    return OfferFilter;
+
+})(jQuery);
+
+var OfferFilter = (function($) {
+
+    'use strict';
+
+    function OfferFilter() {
+        this.skip = 0;
+        this.limit = 10;
+    }
+
+    return OfferFilter;
+
+})(jQuery);
