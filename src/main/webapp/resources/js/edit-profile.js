876a417848069810f534570830bb7d2936f12608
Alexander <alexdubitandum@gmail.com>
2016-03-14 15:33:06 +0200

    social network input fix

--- src/main/webapp/resources/js/edit-profile.js
+++ src/main/webapp/resources/js/edit-profile.js
@@ -78,7 +78,9 @@ function initializeProfileEntityForUpdate() {
     $(".soc-input").each(function (index) {
         if ($(this).parent().hasClass('show-inp')) {
             var socName = $(this).attr("name");
-            socArr[socName] = $(this).val();
+            if ($(this).val() != '') {
+                socArr[socName] = $(this).val();
+            }
         }
     });
     updatedProfile.contact.socNetLink = socArr;
@@ -272,7 +274,7 @@ $('#addProfileImg').on('click', function () {
 
 $('#uploadProfilePhotoInput').on('change', function () {
     var files = event.currentTarget.files;
-    var reader  = new FileReader();
+    var reader = new FileReader();
 
     reader.addEventListener("load", function () {
         cropper.replace(reader.result);
@@ -282,7 +284,7 @@ $('#uploadProfilePhotoInput').on('change', function () {
         reader.readAsDataURL(files[0]);
     }
 
-    $('#cropperModal').css('display',"block");
+    $('#cropperModal').css('display', "block");
 });
 // ------------------------------------------- End photo upload block ---------------------------------
 
@@ -297,23 +299,23 @@ $('#uploadProfilePhotoInput').on('change', function () {
 var image = document.getElementById('cropper-image');
 var cropper = new Cropper(image, {
     aspectRatio: 1 / 1,
-    crop: function(data) {
+    crop: function (data) {
     }
 });
 
-$(".cropper-btn-cancel").click(function() {
-    $('#cropperModal').css('display',"none");
+$(".cropper-btn-cancel").click(function () {
+    $('#cropperModal').css('display', "none");
 });
 
-$(window).click(function(event) {
+$(window).click(function (event) {
     var modal = document.getElementById('cropperModal');
     if (event.target == modal) {
         modal.style.display = "none";
     }
 });
 
-$(".cropper-btn-success").click(function() {
-    $('#cropperModal').css('display',"none");
+$(".cropper-btn-success").click(function () {
+    $('#cropperModal').css('display', "none");
 
     var canvas = cropper.getCroppedCanvas();
     var dataURL = canvas.toDataURL('image/jpeg', 0.5);
@@ -338,7 +340,7 @@ $(".cropper-btn-success").click(function() {
                 updatedProfile.imgId = data.id;
                 $('.moreInformation-img').css('background',
                     'url(/api/rest/fileStorage/profile/file/read/id/' + updatedProfile.imgId + ') no-repeat center center')
-                    .css('background-size','cover');
+                    .css('background-size', 'cover');
                 cropper.replace('/api/rest/fileStorage/profile/file/read/id/' + updatedProfile.imgId);
             },
             400: function () {
@@ -351,7 +353,7 @@ $(".cropper-btn-success").click(function() {
 function dataURItoBlob(dataURI) {
     var binary = atob(dataURI.split(',')[1]);
     var array = [];
-    for(var i = 0; i < binary.length; i++) {
+    for (var i = 0; i < binary.length; i++) {
         array.push(binary.charCodeAt(i));
     }
     return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
