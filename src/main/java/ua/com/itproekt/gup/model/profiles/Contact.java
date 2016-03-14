40969fbef97f14cecaea0b3178295e9f2dcf6e25
Sasha Minchuck <minchuck.sasha@gmail.com>
2016-03-14 18:49:21 +0200

    *

--- src/main/java/ua/com/itproekt/gup/model/profiles/Contact.java
+++ src/main/java/ua/com/itproekt/gup/model/profiles/Contact.java
@@ -25,76 +25,7 @@ public class Contact {
     private Set<String> contactPhones;
     private Map<SocialNetwork, String> socNetLink;
 
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public String getLinkToWebSite() {
-        return linkToWebSite;
-    }
-
-    public void setLinkToWebSite(String linkToWebSite) {
-        this.linkToWebSite = linkToWebSite;
-    }
-
-    public UserType getType() {
-        return type;
-    }
-
-    public void setType(UserType type) {
-        this.type = type;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public Set<String> getContactEmails() {
-        return contactEmails;
-    }
-
-    public void setContactEmails(Set<String> contactEmails) {
-        this.contactEmails = contactEmails;
-    }
-
-    public Set<String> getContactPhones() {
-        return contactPhones;
-    }
-
-    public void setContactPhones(Set<String> contactPhones) {
-        this.contactPhones = contactPhones;
-    }
-
-    public Map<SocialNetwork, String> getSocNetLink() {
-        return socNetLink;
-    }
-
-    public void setSocNetLink(Map<SocialNetwork, String> socNetLink) {
-        this.socNetLink = socNetLink;
-    }
-
-    public String getSkypeUserName() {
-        return skypeUserName;
-    }
-
-    public void setSkypeUserName(String skypeUserName) {
-        this.skypeUserName = skypeUserName;
-    }
-
-    public String getAboutUs() {
-        return aboutUs;
-    }
-
-    public void setAboutUs(String aboutUs) {
-        this.aboutUs = aboutUs;
+    public Contact() {
     }
 
     public Nace getNace() {
@@ -121,21 +52,76 @@ public class Contact {
         this.naceId = naceId;
     }
 
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "type=" + type +
-                ", position='" + position + '\'' +
-                ", contactEmails=" + contactEmails +
-                ", contactPhones=" + contactPhones +
-                ", socNetLink=" + socNetLink +
-                ", skypeUserName='" + skypeUserName + '\'' +
-                ", linkToWebSite=" + linkToWebSite +
-                ", aboutUs='" + aboutUs + '\'' +
-                ", nace=" + nace +
-                ", member=" + member +
-                ", naceId=" + naceId +
-                '}';
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getAboutUs() {
+        return aboutUs;
+    }
+
+    public void setAboutUs(String aboutUs) {
+        this.aboutUs = aboutUs;
+    }
+
+    public String getSkypeUserName() {
+        return skypeUserName;
+    }
+
+    public void setSkypeUserName(String skypeUserName) {
+        this.skypeUserName = skypeUserName;
+    }
+
+    public String getLinkToWebSite() {
+        return linkToWebSite;
+    }
+
+    public void setLinkToWebSite(String linkToWebSite) {
+        this.linkToWebSite = linkToWebSite;
+    }
+
+    public Set<String> getContactEmails() {
+        return contactEmails;
+    }
+
+    public void setContactEmails(Set<String> contactEmails) {
+        this.contactEmails = contactEmails;
+    }
+
+    public Set<String> getContactPhones() {
+        return contactPhones;
+    }
+
+    public void setContactPhones(Set<String> contactPhones) {
+        this.contactPhones = contactPhones;
+    }
+
+    public Map<SocialNetwork, String> getSocNetLink() {
+        return socNetLink;
+    }
+
+    public void setSocNetLink(Map<SocialNetwork, String> socNetLink) {
+        this.socNetLink = socNetLink;
     }
 }
 
