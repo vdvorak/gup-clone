5ff81c2e59264685db0bbe62494baa9b8fc57caa
Alexander <alexdubitandum@gmail.com>
2016-03-18 16:41:45 +0200

    *

--- src/main/webapp/resources/js/tender.js
+++ src/main/webapp/resources/js/tender.js
@@ -120,12 +120,12 @@ $.ajax({
 
 
 $(document).ready(function () {
-    $('.slider1').bxSlider({
-        slideWidth: 200,
-        minSlides: 2,
-        maxSlides: 3,
-        slideMargin: 5
-    });
+    //$('.slider1').bxSlider({
+    //    slideWidth: 200,
+    //    minSlides: 2,
+    //    maxSlides: 3,
+    //    slideMargin: 5
+    //});
 });
 
 // ----------------- BEGIN Propose sent -------------------------------------------------
