fd56240a2df332dde922c98d6222e7a3406c0a28
YuliiaMarchuk <fairy.freyja@gmail.com>
2016-03-22 20:34:26 +0200

    comment old emailService stuff

--- src/main/java/ua/com/itproekt/gup/service/emailnotification/EmailService.java
+++ src/main/java/ua/com/itproekt/gup/service/emailnotification/EmailService.java
@@ -1,8 +1,8 @@
-package ua.com.itproekt.gup.service.emailnotification;
-
-/**
- * Created by Комп2 on 17.11.2015.
- */
-public interface EmailService {
-    String sendEmail(String to, String subject, String msg);
-}
+//package ua.com.itproekt.gup.service.emailnotification;
+//
+///**
+// * Created by Комп2 on 17.11.2015.
+// */
+//public interface EmailService {
+//    String sendEmail(String to, String subject, String msg);
+//}
