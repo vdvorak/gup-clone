e6a664b461085f7b9a9a607f5af34fa5ec2faa64
Alexander <alexdubitandum@gmail.com>
2016-04-27 11:38:03 +0300

    tender close event sender

--- src/main/java/ua/com/itproekt/gup/api/rest/tender/TenderRestController.java
+++ src/main/java/ua/com/itproekt/gup/api/rest/tender/TenderRestController.java
@@ -33,10 +33,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.beans.PropertyEditorSupport;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 @RestController
@@ -72,7 +69,7 @@ public class TenderRestController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
-        if(getCurrentUser() == null){
+        if (getCurrentUser() == null) {
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
 
@@ -105,14 +102,14 @@ public class TenderRestController {
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<EntityPage<Tender>> listOfAllTender(@RequestBody TenderFilterOptions tenderFilterOptions) {
-        if(tenderFilterOptions.isJustUsersNace()){
+        if (tenderFilterOptions.isJustUsersNace()) {
             tenderFilterOptions.setNaceIdIn(getCurrentUserNaceId());
         }
 
         Profile profile;
         profile = getCurrentUser();
 
-        if(profile == null){
+        if (profile == null) {
             tenderFilterOptions.setType(TenderType.OPEN);
             profile = new Profile();
         }
@@ -151,11 +148,11 @@ public class TenderRestController {
             consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CreatedObjResp> createTender(@RequestBody Tender tender) {
 
-        if (!SecurityOperations.isUserLoggedIn()){
+        if (!SecurityOperations.isUserLoggedIn()) {
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
         tender.setAuthorId(SecurityOperations.getLoggedUserId());
-        if(tender.getType() == TenderType.CLOSE){
+        if (tender.getType() == TenderType.CLOSE) {
             sendActivityFeedToMembers(tender);
         }
         tender.setHideContact(false);
@@ -200,7 +197,7 @@ public class TenderRestController {
         if (userType == null || userType == UserType.INDIVIDUAL) {
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
-        if (tender.getMembers().add(member) && tender.getType() == TenderType.CLOSE){
+        if (tender.getMembers().add(member) && tender.getType() == TenderType.CLOSE) {
             activityFeedService.createEvent(new Event(member.getId(), EventType.YOU_HAVE_BEEN_ADDED_TO_CLOSE_TENDER, tender.getId(), tender.getAuthorId()));
         }
         newTender.setMembers(tender.getMembers());
@@ -226,13 +223,29 @@ public class TenderRestController {
         if (!newTender.getUploadFilesIds().isEmpty()) {
             newTender.getUploadFilesIds().putAll(files);
         }
-        if(newTender.getWinnerId() != null && newTender.getWinnerId().length() > 0){
+        if (newTender.getWinnerId() != null && newTender.getWinnerId().length() > 0) {
             activityFeedService.createEvent(new Event(newTender.getWinnerId(), EventType.YOU_WON_IN_TENDER, tender.getId(), null, tender.getAuthorId()));
             tender.setEnd(LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli());
         }
-        if(tender.getType() == TenderType.CLOSE && !newTender.getMembers().isEmpty()){
-            newTender.getMembers().stream().forEach(m -> activityFeedService.createEvent(new Event(m.getId(), EventType.YOU_HAVE_BEEN_ADDED_TO_CLOSE_TENDER, tender.getId(), null, tender.getAuthorId())));
+
+
+        if (newTender.getType() == TenderType.CLOSE) {
+            if (!tender.getMembers().isEmpty() && !newTender.getMembers().isEmpty()) {
+                sendActivityFeedToMembersAfterUpdate(tender, newTender);
+            } else {
+                if (!newTender.getMembers().isEmpty()) {
+                    sendActivityFeedToMembers(newTender);
+                }
+            }
         }
+
+
+        //ToDo Delete if new method will work
+//        if(tender.getType() == TenderType.CLOSE && !newTender.getMembers().isEmpty()){
+//            newTender.getMembers().stream().forEach(m -> activityFeedService.createEvent(new Event(m.getId(), EventType.YOU_HAVE_BEEN_ADDED_TO_CLOSE_TENDER, tender.getId(), null, tender.getAuthorId())));
+//        }
+
+
         tenderService.updateTender(newTender);
 
         HttpHeaders headers = new HttpHeaders();
@@ -243,7 +256,7 @@ public class TenderRestController {
     @RequestMapping(value = "/tender/chooseWinner",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Tender> winnerChoose (@RequestBody Tender tender) {
+    public ResponseEntity<Tender> winnerChoose(@RequestBody Tender tender) {
         String id = tenderService.findById(tender.getId()).getAuthorId();
         if (SecurityOperations.getLoggedUserId() == null || !SecurityOperations.getLoggedUserId().equals(id)) {
             return new ResponseEntity<Tender>(HttpStatus.FORBIDDEN);
@@ -286,7 +299,7 @@ public class TenderRestController {
 
     @RequestMapping(value = "/tender/userislegal/{email}",
             method = RequestMethod.POST)
-    public Boolean isLegal(@PathVariable("email")String email) {
+    public Boolean isLegal(@PathVariable("email") String email) {
         // check type of user. Only LEGAL_ENTITY or ENTREPRENEUR can became an member;
         UserType userType = profileService.findProfileByEmail(email).getContact().getType();
         if (userType == null || userType == UserType.INDIVIDUAL) {
@@ -297,10 +310,10 @@ public class TenderRestController {
 
     @RequestMapping(value = "/tender/{id}/user-check",
             method = RequestMethod.POST)
-    public Boolean canBeMember (@PathVariable("id")String id) {
+    public Boolean canBeMember(@PathVariable("id") String id) {
         // check type of user. Only LEGAL_ENTITY or ENTREPRENEUR can became an member;
         Profile p = profileService.findById(SecurityOperations.getLoggedUserId());
-        if(p == null || p.getContact() == null){
+        if (p == null || p.getContact() == null) {
             return false;
         }
         UserType userType = p.getContact().getType();
@@ -328,17 +341,17 @@ public class TenderRestController {
 
     private String getCurrentUserId() {
         Profile user = getCurrentUser();
-        if(user == null || user.getId() == null) return null;
+        if (user == null || user.getId() == null) return null;
         return user.getId();
     }
 
     private Profile getCurrentUser() {
         SecurityContext context = SecurityContextHolder.getContext();
-        if(context == null) return null;
+        if (context == null) return null;
         Authentication authentication = context.getAuthentication();
-        if(authentication == null) return null;
+        if (authentication == null) return null;
         String email = authentication.getName(); //get logged in username
-        if(email == null) return null;
+        if (email == null) return null;
         return profileService.findProfileByEmail(email);
     }
 
@@ -362,12 +375,12 @@ public class TenderRestController {
 
     private List<NACE> getCurrentUserNace() {
         Profile user = getCurrentUser();
-        if(user == null || user.getContact() == null || user.getContact().getNaceId() == null) return null;
+        if (user == null || user.getContact() == null || user.getContact().getNaceId() == null) return null;
         List<String> nacesId = user.getContact().getNaceId();
         List<NACE> naces = new ArrayList<>();
         for (String id : nacesId) {
             NACE nace = naceService.findById(id);
-            if(nace != null) {
+            if (nace != null) {
                 naces.add(naceService.findById(id));
             }
         }
@@ -381,8 +394,27 @@ public class TenderRestController {
     }
 
     private void sendActivityFeedToMembers(Tender tender) {
-        for(Member m: tender.getMembers()){
+        for (Member m : tender.getMembers()) {
             activityFeedService.createEvent(new Event(m.getId(), EventType.YOU_HAVE_BEEN_ADDED_TO_CLOSE_TENDER, tender.getId(), tender.getAuthorId()));
         }
+    }
+
+    private void sendActivityFeedToMembersAfterUpdate(Tender oldTender, Tender newTender) {
+        Set<String> oldMembers = null, newMembers = null, resultList = null;
+        for (Member m : oldTender.getMembers()) {
+            oldMembers.add(m.getId());
+        }
+        for (Member m : newTender.getMembers()) {
+            newMembers.add(m.getId());
+        }
+
+            newMembers.removeAll(oldMembers);
+
+        for (String newMemberId : newMembers) {
+            activityFeedService.createEvent(new Event(newMemberId, EventType.YOU_HAVE_BEEN_ADDED_TO_CLOSE_TENDER, newTender.getId(), newTender.getAuthorId()));
+        }
+
+
+
     }
 }
