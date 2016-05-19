da803ec96f87585a4dc6623dc5eff612a56721d3
Julia <Julia>
2016-05-19 11:40:00 +0300

    refactoring

--- src/main/webapp/resources/js/offer-all.js
+++ src/main/webapp/resources/js/offer-all.js
@@ -1,35 +1,35 @@
 /**
  * Created by Юля on 19.05.2016.
  */
-var offerFilter = window.OfferFilter;
+var offers = window.offers;
 
 $(document).ready(function () {
-    offerFilter.parseUrlToFilter()
+    offers.parseUrlToFilter()
         .readAllByFilter();
 
-    var categories = offerFilter.utils.categories;
+    var categories = offers.utils.categories;
     if (categories && categories.length) {
         $.when(window.loadOptions, window.loadParameters).done(function () {
-            offerFilter.drawFilterOptions(categories[categories.length - 1]);
+            offers.drawFilterOptions(categories[categories.length - 1]);
         })
         $.when(window.loadSubcategories).done(function () {
-            if (categories.length > 1) offerFilter.drawCategories3lvl();
+            if (categories.length > 1) offers.drawCategories3lvl();
         })
     }
 
-    var address = offerFilter.utils.address;
+    var address = offers.utils.address;
     if (address.area) $('#input-selected-area').val(address.area);
     if (address.city) $('#input-selected-city').val(address.city);
-    offerFilter.generateFilterRegionString();
+    offers.generateFilterRegionString();
 
-    $('#btn-offers-more').click(offerFilter.submitFilter);
+    $('#btn-offers-more').click(offers.submitFilter);
 
     $(window).on('scroll', function () {
         if ($(window).scrollTop() >= $('#btn-offers-more').offset().top + $('#btn-offers-more').outerHeight() - window.innerHeight) {
-            $('#btn-offers-more').trigger('click', offerFilter.submitFilter);
+            $('#btn-offers-more').trigger('click', offers.submitFilter);
         }
     });
 
-    $('#select-categories-3lvl').change(offerFilter.selectCategoryLvl3);
-    $('#filter-price').change(offerFilter.selectFilterPrice);
+    $('#select-categories-3lvl').change(offers.selectCategoryLvl3);
+    $('#filter-price').change(offers.selectFilterPrice);
 });
--- src/main/webapp/resources/js/search-bar.js
+++ src/main/webapp/resources/js/search-bar.js
@@ -90,22 +90,22 @@ $('#newsFilterBlock').find('.categoriesItem').each(function() {
     })
 })
 // ------------------------------------ OFFER FILTER BEGIN ---------------------------------------------------//
-    var offerFilter = window.OfferFilter;
+    var offers = window.offers;
 
     $('.ItemADS').each(function () {
-        $(this).children('a:first').click(offerFilter.onClickCategory1lvl);
+        $(this).children('a:first').click(offers.onClickCategory1lvl);
     })
     $.when(window.loadCategories).done(function(){
-        $('.ItemADS div a').click(offerFilter.onClickCategory2lvl);
+        $('.ItemADS div a').click(offers.onClickCategory2lvl);
     })
 
-    $('#input-region-search').click(offerFilter.showFilterRegionMenu);
-    $('#filter-area-box').find('div.filter-region-elem').click(offerFilter.selectFilterArea);
-    $('#filter-region-nav a:eq(0)').click(offerFilter.allUkraineRegionClick);
-    $('#filter-region-nav a:eq(1)').click(offerFilter.initFilterRegionMenu);
-    $('#filter-region-nav a:eq(2)').click(offerFilter.selectAllCities);
-    $('#filter-icon-close').click(offerFilter.deleteFilterRegion);
-    $('#btn-offers-search').click(offerFilter.submitFilter);
+    $('#input-region-search').click(offers.showFilterRegionMenu);
+    $('#filter-area-box').find('div.filter-region-elem').click(offers.selectFilterArea);
+    $('#filter-region-nav a:eq(0)').click(offers.allUkraineRegionClick);
+    $('#filter-region-nav a:eq(1)').click(offers.initFilterRegionMenu);
+    $('#filter-region-nav a:eq(2)').click(offers.selectAllCities);
+    $('#filter-icon-close').click(offers.deleteFilterRegion);
+    $('#btn-offers-search').click(offers.submitFilter);
 
 // ------------------------------------ OFFER FILTER END ---------------------------------------------------//
 
