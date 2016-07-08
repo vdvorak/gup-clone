54e0838bbfebd690196ddba0acac617c4102e262
Optionix <alexdubitandum@gmail.com>
2016-07-08 11:29:20 +0400

    Subscription filter options impl

--- offers/src/main/java/ua/com/itproekt/gup/dao/subscription/SubscriptionRepository.java
+++ offers/src/main/java/ua/com/itproekt/gup/dao/subscription/SubscriptionRepository.java
@@ -2,6 +2,8 @@ package ua.com.itproekt.gup.dao.subscription;
 
 
 import ua.com.itproekt.gup.model.subscription.Subscription;
+import ua.com.itproekt.gup.model.subscription.filter.SubscriptionFilterOptions;
+import ua.com.itproekt.gup.util.EntityPage;
 
 public interface SubscriptionRepository {
     /**
@@ -26,4 +28,10 @@ public interface SubscriptionRepository {
      * @return the int
      */
     int delete(String subscriptionId);
+
+    /**
+     * @param subscriptionFilterOptions subscription filter options
+     * @return List of subscriptions satisfying the filter
+     */
+    EntityPage<Subscription> findWithFilterOption(SubscriptionFilterOptions subscriptionFilterOptions);
 }
--- offers/src/main/java/ua/com/itproekt/gup/service/subscription/SubscriptionService.java
+++ offers/src/main/java/ua/com/itproekt/gup/service/subscription/SubscriptionService.java
@@ -3,10 +3,12 @@ package ua.com.itproekt.gup.service.subscription;
 
 import ua.com.itproekt.gup.model.offer.filter.OfferFilterOptions;
 import ua.com.itproekt.gup.model.subscription.Subscription;
+import ua.com.itproekt.gup.model.subscription.filter.SubscriptionFilterOptions;
+import ua.com.itproekt.gup.util.EntityPage;
 
 public interface SubscriptionService {
     /**
-     * @param userId       userId
+     * @param userId             userId
      * @param offerFilterOptions the offerFilterOptions
      */
     void create(String userId, OfferFilterOptions offerFilterOptions);
@@ -28,4 +30,11 @@ public interface SubscriptionService {
      * @return the int
      */
     int delete(String subscriptionId);
+
+    /**
+     * @param subscriptionFilterOptions subscription filter options
+     * @return List of subscriptions satisfying the filter
+     */
+    EntityPage<Subscription> findWithFilterOption(SubscriptionFilterOptions subscriptionFilterOptions);
+
 }
