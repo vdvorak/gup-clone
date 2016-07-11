e0d2f8a52a7e0277ff6cea74dafd7d819c3471b1
lazyexpert <lazyepxert@i.ua>
2016-07-11 12:27:35 +0300

    ripple effect ready

--- frontend-dev/modules/ripple.js
+++ frontend-dev/modules/ripple.js
@@ -2,14 +2,14 @@
 
 /*
   Usage:
-  <button class="blue">Click me!</button>
-  <button class="grey">Click me!</button>
+  <div class="btn-blue">Click me!</div>
+  <div class="btn-grey">Click me!</div>
 */
 module.exports = function() {
   let buttons = []
 
-  let blue = document.querySelectorAll("button.blue"),
-      grey = document.querySelectorAll("button.grey")
+  let blue = document.querySelectorAll(".btn-blue"),
+      grey = document.querySelectorAll(".btn-grey")
 
   Array.prototype.forEach.call(blue, el => buttons.push(el))
   Array.prototype.forEach.call(grey, el => buttons.push(el))
@@ -19,13 +19,15 @@ module.exports = function() {
     let ink = parent.getElementsByClassName('ink')[0]
     ink.classList.remove('animate')
 
+    let rect = parent.getBoundingClientRect()
+
     if( !ink.clientHeight && !ink.clientWidth ) {
       let d = Math.max(parent.clientWidth, parent.clientHeight)
       ink.style.height = ink.style.width = `${d}px`
     }
 
-    ink.style.top = `${event.pageY - parent.offsetTop - ink.clientHeight/2}px`
-    ink.style.left = `${event.pageX - parent.offsetLeft - ink.clientWidth/2}px`
+    ink.style.top = `${event.pageY - rect.top - ink.clientHeight/2}px`
+    ink.style.left = `${event.pageX - rect.left - ink.clientWidth/2}px`
     ink.classList.add('animate')
   }
 
@@ -40,6 +42,7 @@ module.exports = function() {
     let p = document.createElement('p')
     p.innerHTML = text
     p.addEventListener("click", handler)
+    p.classList.add('ripple')
     el.appendChild(p)
   }
 
