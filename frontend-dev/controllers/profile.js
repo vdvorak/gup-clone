d176d41e738601945e7058390b17c27df4674a4e
Julia <Julia>
2016-07-13 18:19:47 +0300

    profile

--- frontend-dev/controllers/profile.js
+++ frontend-dev/controllers/profile.js
@@ -1,1 +1,33 @@
+"use strict";
 
+class ProfileContact{
+    constructor() {
+        this.contactEmails = [''];
+        this.contactPhones = [''];
+    }
+}
+
+class profileCtrl {
+    constructor(){
+        this.contact = new ProfileContact();
+    }
+    updateProfile(){
+
+    }
+    addContacts($event, type){
+        var arr;
+        if(type === 'email') arr = this.contact.contactEmails;
+        else if(type === 'phone') arr = this.contact.contactPhones;
+        else return;
+
+        if(arr.length < 5 && arr[arr.length - 1].trim()) arr.push('');
+    }
+
+    deleteContacts($event, $index, type){
+        if(type === 'email') this.contact.contactEmails.splice($index, 1);
+        else if(type === 'phone') this.contact.contactPhones.splice($index, 1);
+        else return;
+    }
+}
+
+module.exports = profileCtrl;
