abd0b0daf9d96183c881217ae43fbe81f9e43ce4
Sasha <skmets@gmail.com>
2016-07-21 12:24:07 +0300

    coverage tests to OAuth service

--- oauth/src/main/java/ua/com/itproekt/gup/server/api/OauthAPI.java
+++ oauth/src/main/java/ua/com/itproekt/gup/server/api/OauthAPI.java
@@ -1,32 +1,32 @@
-package ua.com.itproekt.gup.server.api;
-
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-
-@RestController
-public class OauthAPI {
-
-    @CrossOrigin
-    @RequestMapping(value = "/loginTest", method = RequestMethod.POST)
-    @ResponseBody
-    protected HashMap<String, String> loginTest() {
-        HashMap<String, String> model = new HashMap<>();
-        String name = "LoginTest";
-        model.put("oauth", name);
-
-        return model;
-    }
-
-    @CrossOrigin
-    @RequestMapping(value = "/logoutTest", method = RequestMethod.POST)
-    @ResponseBody
-    protected HashMap<String, String> logoutTest() {
-        HashMap<String, String> model = new HashMap<>();
-        String name = "LogoutTest";
-        model.put("oauth", name);
-
-        return model;
-    }
-
-}
+//package ua.com.itproekt.gup.server.api;
+//
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.HashMap;
+//
+//@RestController
+//public class OauthAPI {
+//
+//    @CrossOrigin
+//    @RequestMapping(value = "/loginTest", method = RequestMethod.POST)
+//    @ResponseBody
+//    protected HashMap<String, String> loginTest() {
+//        HashMap<String, String> model = new HashMap<>();
+//        String name = "LoginTest";
+//        model.put("oauth", name);
+//
+//        return model;
+//    }
+//
+//    @CrossOrigin
+//    @RequestMapping(value = "/logoutTest", method = RequestMethod.POST)
+//    @ResponseBody
+//    protected HashMap<String, String> logoutTest() {
+//        HashMap<String, String> model = new HashMap<>();
+//        String name = "LogoutTest";
+//        model.put("oauth", name);
+//
+//        return model;
+//    }
+//
+//}
