25df47989f86960dfebb4f0bc758183550e268bf
robertototti <romkomaniya@gmail.com>
2016-08-02 18:02:56 +0300

    fix

--- frontend-dev/directives/bulletinList.js
+++ frontend-dev/directives/bulletinList.js
@@ -8,8 +8,15 @@ module.exports = function() {
     },
     templateUrl: "partials/bulletin-list.html",
     controller : function($scope, $element) {
-      console.log($scope.items)
-      console.log("I'm a cool directive controller ;D")
+      console.log($scope.items);
+      console.log("I'm a cool directive controller ;D");
+			this.list = [
+				{
+					"title": "Hotwheels недорого по запчастям",
+					"category": "Детские игрушки",
+					"description": "Lorem ipsum dolor sit amet, ne quod novum mei. Sea omnium invenire mediocrem at, in lobortis conclusionemque nam.",
+				}
+			];
     },
     controllerAs: "blist"
   }
