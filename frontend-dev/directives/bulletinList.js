62d4c29f3f0d7968e11ab395a263d1ed7c2ed61d
robertototti <romkomaniya@gmail.com>
2016-08-03 10:56:18 +0300

    fix

--- frontend-dev/directives/bulletinList.js
+++ frontend-dev/directives/bulletinList.js
@@ -15,6 +15,57 @@ module.exports = function() {
 					"title": "Hotwheels недорого по запчастям",
 					"category": "Детские игрушки",
 					"description": "Lorem ipsum dolor sit amet, ne quod novum mei. Sea omnium invenire mediocrem at, in lobortis conclusionemque nam.",
+					myImage: {
+						"background" : "url(../images/vk.png)"
+    			},
+					"price": "2 345 грн",
+					"onOrOffLineUser": "green",
+					"exclamationPoint": true,
+					"dollarBill": true,
+					"action": "Искать похожие",
+					"stock": "stock"
+				},
+				{
+					"title": "Hotwheels недорого по запчастям",
+					"category": "Детские игрушки",
+					"description": "Lorem ipsum dolor sit amet, ne quod novum mei. Sea omnium invenire mediocrem at, in lobortis conclusionemque nam.",
+					myImage: {
+						"background" : "url(../images/facebook.png)"
+    			},
+					"price": "2 345 грн",
+					"onOrOffLineUser": "red",
+					"exclamationPoint": false,
+					"dollarBill": false,
+					"action": "Искать похожие",
+					"stock": ""
+				},
+				{
+					"title": "Hotwheels недорого по запчастям",
+					"category": "Детские игрушки",
+					"description": "Lorem ipsum dolor sit amet, ne quod novum mei. Sea omnium invenire mediocrem at, in lobortis conclusionemque nam.",
+					myImage: {
+						"background" : "url(../images/google.png)"
+    			},
+					"price": "2 345 грн",
+					"onOrOffLineUser": "red",
+					"exclamationPoint": false,
+					"dollarBill": false,
+					"action": "Искать похожие",
+					"stock": ""
+				},
+				{
+					"title": "Hotwheels недорого по запчастям",
+					"category": "Детские игрушки",
+					"description": "Lorem ipsum dolor sit amet, ne quod novum mei. Sea omnium invenire mediocrem at, in lobortis conclusionemque nam.",
+					myImage: {
+						"background" : "url(../images/addCalendar.png)"
+    			},
+					"price": "2 345 грн",
+					"onOrOffLineUser": "red",
+					"exclamationPoint": false,
+					"dollarBill": false,
+					"action": "Искать похожие",
+					"stock": ""
 				}
 			];
     },
