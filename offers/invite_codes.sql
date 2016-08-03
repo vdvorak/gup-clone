a6e270838e38a6043e53788d8caa332b495a0925
Sasha <skmets@gmail.com>
2016-08-03 14:23:44 +0300

    fix script for invite-code(s)

--- offers/invite_codes.sql
+++ offers/invite_codes.sql
@@ -2,7 +2,7 @@ CREATE TYPE AMOUNT AS ENUM('50','100','500');
 
 CREATE TABLE invite_codes(
   id  SERIAL PRIMARY KEY,
-  code           CHAR(50),
+  code           TEXT,
   amount         AMOUNT,
   user_id        CHAR(50) NULL,
   date           DATE NOT NULL DEFAULT CURRENT_DATE,
@@ -12,14 +12,15 @@ CREATE TABLE invite_codes(
 CREATE OR REPLACE FUNCTION generate_invite_codes(count INTEGER, amount AMOUNT) RETURNS TEXT AS
   $$
   DECLARE
-     chars   TEXT[] := '{0,1,2,3,4,5,6,7,8,9}';
-     code  CHAR(50) := '';
-     i    INTEGER   := 0;
+    chars TEXT[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+    code  TEXT   := '';
+    i  INTEGER   := 0;
 
   BEGIN
     FOR c IN 1 .. count LOOP
+      code := '';
       FOR i IN 1..50 LOOP
-        code := chars[1+random()*(array_length(chars, 1)-1)];
+        code := code || chars[1+random()*(array_length(chars, 1)-1)];
       END LOOP;
       INSERT INTO invite_codes (code, amount) VALUES (code, amount);
     END LOOP;
