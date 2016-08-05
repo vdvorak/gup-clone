bd21891440bf2e66330d28e30acdbcf66e116b66
Optionix <alexdubitandum@gmail.com>
2016-08-05 15:41:53 +0300

    remove some paid services

--- core/src/main/java/ua/com/itproekt/gup/model/offer/paidservices/PaidServices.java
+++ core/src/main/java/ua/com/itproekt/gup/model/offer/paidservices/PaidServices.java
@@ -6,61 +6,17 @@ import java.time.ZoneOffset;
 
 public class PaidServices {
 
-    private Boolean isTariffPremium;
-    private Boolean isTariffBusiness;
-    private Long tariffPeriod;
-    private PeriodName nameOfTariffDuration;
-
-
     private Boolean isMarked; // marked offer forever
     private Boolean isUrgent; // marked offer with "urgent"
     private Boolean isCheaper; // marked offer with "cheaper"
 
     private Long lastPaidUpdateDate; // if offer must be on the top, value of this variable must be from the future
-
-
+    
     public PaidServices setLastUpdateDateToCurrentDate() {
         this.lastPaidUpdateDate = LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli();
         return this;
     }
 
-
-    public Boolean getIsTariffPremium() {
-        return isTariffPremium;
-    }
-
-    public PaidServices setIsTariffPremium(Boolean isTariffPremium) {
-        this.isTariffPremium = isTariffPremium;
-        return this;
-    }
-
-    public Boolean getIsTariffBusiness() {
-        return isTariffBusiness;
-    }
-
-    public PaidServices setIsTariffBusiness(Boolean isTariffBusiness) {
-        this.isTariffBusiness = isTariffBusiness;
-        return this;
-    }
-
-
-    public Long getTariffPeriod() {
-        return tariffPeriod;
-    }
-
-    public PaidServices setTariffPeriod(Long tariffPeriod) {
-        this.tariffPeriod = tariffPeriod;
-        return this;
-    }
-
-    public PeriodName getNameOfTariffDuration() {
-        return nameOfTariffDuration;
-    }
-
-    public PaidServices setNameOfTariffDuration(PeriodName nameOfTariffDuration) {
-        this.nameOfTariffDuration = nameOfTariffDuration;
-        return this;
-    }
 
     public Boolean getIsMarked() {
         return isMarked;
@@ -98,15 +54,10 @@ public class PaidServices {
         return this;
     }
 
-
     @Override
     public String toString() {
         return "PaidServices{" +
-                "isTariffPremium=" + isTariffPremium +
-                ", isTariffBusiness=" + isTariffBusiness +
-                ", tariffPeriod=" + tariffPeriod +
-                ", nameOfTariffDuration=" + nameOfTariffDuration +
-                ", isMarked=" + isMarked +
+                "isMarked=" + isMarked +
                 ", isUrgent=" + isUrgent +
                 ", isCheaper=" + isCheaper +
                 ", lastPaidUpdateDate=" + lastPaidUpdateDate +
