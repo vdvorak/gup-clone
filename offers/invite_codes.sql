422dbbac46e486d4302eb8d0ffc38787ff183f73
Sasha <skmets@gmail.com>
2016-08-07 14:50:10 +0300

    fix the sql-function to invite-code

--- offers/invite_codes.sql
+++ offers/invite_codes.sql
@@ -1,7 +1,7 @@
 CREATE TYPE AMOUNT AS ENUM('50','100','500');
 
 
-CREATE TABLE bobus(
+CREATE TABLE bonus(
   user_id CHAR(50) PRIMARY KEY,
   amount  INT DEFAULT 0
 );
@@ -10,7 +10,7 @@ CREATE TABLE invite_codes(
   id             SERIAL PRIMARY KEY,
   code           TEXT,
   amount         AMOUNT,
-  user_id        CHAR(50) REFERENCES bobus(user_id),
+  user_id        CHAR(50) REFERENCES bonus(user_id),
   create_date    DATE NOT NULL DEFAULT CURRENT_DATE,
   activate_date  DATE DEFAULT NULL
 );
@@ -40,62 +40,61 @@ SELECT fn_generate_invite_code(1000, '50') AS answer;
 SELECT fn_generate_invite_code(1000, '100') AS answer;
 SELECT fn_generate_invite_code(1000, '500') AS answer;
 
-INSERT INTO bobus(user_id) VALUES
+INSERT INTO bonus(user_id) VALUES
   ('57a1f7c72a6a188051a3ba94')
   ,('578f34854c8e46b32797d92b');
-UPDATE bobus SET amount=100 WHERE user_id='57a1f7c72a6a188051a3ba94';
-SELECT * FROM bobus;
+UPDATE bonus SET amount=100 WHERE user_id='57a1f7c72a6a188051a3ba94';
+SELECT * FROM bonus;
 
 UPDATE invite_codes SET user_id='57a1f7c72a6a188051a3ba94', activate_date=CURRENT_DATE WHERE code='BE1B838URoEOdAoSHmQJ4JoNtqDlNgNWO975HGbnEcO1hHZOxg';
 UPDATE invite_codes SET user_id='578f34854c8e46b32797d92b', activate_date=CURRENT_DATE WHERE code='6l4zoQn7AJqV3qjEGN5RgHSIqH6NQm1F8QJqLHecSNsq6WRrKR';
 SELECT * FROM invite_codes;
 
 
-CREATE OR REPLACE FUNCTION fn_activate_invite_code(IN "check_user" TEXT, IN "check_code" TEXT, OUT "status" character varying, OUT "details" character varying) RETURNS SETOF record AS
+CREATE OR REPLACE FUNCTION fn_activate_invite_code(IN "current_user_id" TEXT, IN "current_code" TEXT, OUT "status" character varying, OUT "details" character varying) RETURNS SETOF record AS
   $BODY$
   DECLARE
-    active_code  INTEGER := 0;    -- CODE: NOT FOUND       (-1 | 0 | 1)
-    is_user_code BOOLEAN := TRUE; -- USER: NOT DOUBLE CODE (true | false)
-    invite_amount    INT := 0;
-    currtnt_amount   INT := 0;
+    status_invite_code    INTEGER := 0;     -- CODE: NOT FOUND       (-1 | 0 | 1)
+    is_double_invite_code BOOLEAN := FALSE; -- USER: NOT DOUBLE CODE (TRUE | FALSE)
+    amount_invite_code        INT := 0;
+    amount_bonus              INT := 0;
 
   BEGIN
-    IF (SELECT count(*) FROM invite_codes WHERE user_id=check_user AND code=check_code) > 0 THEN
-      is_user_code := FALSE; -- IS MATCH;
+    IF (SELECT count(*) FROM invite_codes WHERE user_id=current_user_id AND code=current_code) > 0 THEN
+      is_double_invite_code := TRUE; -- CODE: IS DOUBLE;
     END IF;
-
-    IF (SELECT count(*) FROM invite_codes WHERE code=check_code) > 0 THEN
-      IF (SELECT activate_date FROM invite_codes WHERE code=check_code) ISNULL THEN
-        active_code := 1; -- CODE: FREE;
+    IF (SELECT count(*) FROM invite_codes WHERE code=current_code) > 0 THEN
+      IF (SELECT activate_date FROM invite_codes WHERE code=current_code) ISNULL THEN
+        status_invite_code := 1; -- CODE: FREE;
       ELSE
-        active_code := -1; -- CODE: IS USED;
+        status_invite_code := -1; -- CODE: ACTIVATED;
       END IF;
     END IF;
 
-    IF (active_code) = 0 THEN -- если код не найден
+    IF (status_invite_code) = 0 THEN
       status  := 'ERROR';
       details := 'инвайт-код не существует';
     ELSE
-      IF (is_user_code) THEN -- если не дублируется
-        IF (active_code) = 1 THEN -- если код свободен
-          invite_amount := (SELECT amount FROM invite_codes WHERE code=check_code);
-          IF (SELECT count(*) FROM bobus WHERE user_id=check_user) > 0 THEN
-            currtnt_amount := (SELECT amount FROM bobus WHERE user_id=check_user);
-            UPDATE bobus SET amount=(currtnt_amount+invite_amount) WHERE user_id=check_user;
-            UPDATE invite_codes SET user_id=check_user, activate_date=CURRENT_DATE WHERE code=check_code;
+      IF (is_double_invite_code) THEN
+        status  := 'ERROR';
+        details := 'клиент дважды активирует инвайт-код';
+      ELSE
+        IF (status_invite_code) = -1 THEN
+          status  := 'ERROR';
+          details := 'инвайт-код уже зайнят';
+        ELSE
+          amount_invite_code := (SELECT amount FROM invite_codes WHERE code=current_code);
+          IF (SELECT count(*) FROM bonus WHERE user_id=current_user_id) = 0 THEN -- если в таблицу 'bonus' впервые добавляется инвайт-код
+            INSERT INTO bonus(user_id,amount) VALUES (current_user_id,amount_invite_code);
+            UPDATE invite_codes SET user_id=current_user_id, activate_date=CURRENT_DATE WHERE code=current_code;
           ELSE
-            INSERT INTO bobus(user_id,amount) VALUES (check_user,invite_amount);
-            UPDATE invite_codes SET user_id=check_user, activate_date=CURRENT_DATE WHERE code=check_code;
+            amount_bonus := (SELECT amount FROM bonus WHERE user_id=current_user_id);
+            UPDATE bonus SET amount=(amount_bonus+amount_invite_code) WHERE user_id=current_user_id;
+            UPDATE invite_codes SET user_id=current_user_id, activate_date=CURRENT_DATE WHERE code=current_code;
           END IF;
           status  := 'SUCCESS';
           details := 'инвайт-код успешно активирован';
-        ELSE
-          status  := 'ERROR';
-          details := 'инвайт-код уже зайнят';
         END IF;
-      ELSE
-        status  := 'ERROR';
-        details := 'клиент дважды активирует инвайт-код';
       END IF;
     END IF;
     RETURN NEXT;
@@ -115,7 +114,7 @@ SELECT fn_activate_invite_code('1a2a3a4a5a6a7a8a9a0abccd', 'RMpBbHA1Jstn5A9YURw7
 
 
 DROP FUNCTION fn_generate_invite_code("length" INTEGER, "amount" AMOUNT);
-DROP FUNCTION fn_activate_invite_code("check_user" TEXT, "check_code" TEXT);
-DROP TABLE bobus;
+DROP FUNCTION fn_activate_invite_code("current_user_id" TEXT, "current_code" TEXT);
+DROP TABLE bonus;
 DROP TABLE invite_codes;
 DROP TYPE AMOUNT;
