76e215f617307605e2aa258f6da9ea75655b818c
Sasha <skmets@gmail.com>
2016-08-16 13:17:42 +0300

    add TcpNotificationClient

--- offers/src/main/java/ua/com/itproekt/gup/service/socket/TcpNotificationClient.java
+++ offers/src/main/java/ua/com/itproekt/gup/service/socket/TcpNotificationClient.java
@@ -1,50 +1,58 @@
 package ua.com.itproekt.gup.service.socket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Properties;
 
 /**
- * It connects to the server and passes the messages
- */
+* It connects to the server and passes the messages
+*/
 
 @Configuration
-//@PropertySource("classpath:tcp.properties")
+@PropertySource("classpath:properties/tcp.properties")
 public class TcpNotificationClient {
 
-    private TcpNotificationClient instance;
+    @Autowired
+    Environment env;
 
-    private TcpNotificationClient(){
-    }
-
-    public static TcpNotificationClient getInstance(){
-        return new TcpNotificationClient();
-    }
-
-//    @Value("${tcp.server.host}")
-    private String DEFAULT_HOST = "localhost";
-
-//    @Value("${tcp.server.port}")
-    private int DEFAULT_PORT = 3071;
+//    public static void main(String[] args) {
+//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(TcpNotificationClient.class);
+//        TcpNotificationClient tcpNotificationClient = applicationContext.getBean(TcpNotificationClient.class);
+//        try {
+//            tcpNotificationClient.transport("{\"from\":\"TcpNotificationClient\",\"message\":\"This is a String\"}");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     public void transport(String notification) throws IOException {
+        String host = env.getProperty("tcp.server.host");
+        int port = Integer.valueOf(env.getProperty("tcp.server.port"));
+
         /* Create a socket to receive a pair of host:port */
         Socket fromServer = null;
         try {
-            fromServer = new Socket(DEFAULT_HOST, DEFAULT_PORT);
+            fromServer = new Socket(host, port);
         } catch (UnknownHostException e) {
-            throw new UnknownHostException("Unknown host: " + DEFAULT_HOST);
+            throw new UnknownHostException("Unknown host: " + host);
         } catch (IOException e) {
-            throw new IOException("I/O Error creating socket " + DEFAULT_HOST + ":" + DEFAULT_PORT);
+            throw new IOException("I/O Error creating socket " + host + ":" + port);
         }
-        
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(notification.getBytes())));
+
       /* Get the output stream, which will be transmitted through the messages to the server */
         OutputStream out = null;
         try {
@@ -54,9 +62,10 @@ public class TcpNotificationClient {
         }
 
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+        String ln = null;
         try {
-            while (notification != null) {
-                writer.write(notification);
+            while ((ln = reader.readLine()) != null) {
+                writer.write(ln + "\n");
                 writer.flush();
             }
         } catch (IOException e) {
@@ -68,4 +77,3 @@ public class TcpNotificationClient {
     }
 
 }
-
---
+++ offers/src/main/java/ua/com/itproekt/gup/service/socket/TcpNotificationClientTest.java
@@ -0,0 +1,56 @@
+//package ua.com.itproekt.gup.service.socket;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.net.ServerSocket;
+//import java.net.Socket;
+//
+///**
+//* Слушает указанный в первом параметре порт и выводит все приходящие на него сообщения на консоль.
+//*/
+//public class TcpNotificationClientTest {
+//
+//    public static void main(String[] args) {
+//        int port = DEFAULT_PORT;
+//
+//        ServerSocket serverSocket = null;
+//        try {
+//            serverSocket = new ServerSocket(port);
+//        } catch (IOException e) {
+//            System.out.println("занят: " + port);
+//            System.exit(-1);
+//        }
+//
+//        Socket clientSocket = null;
+//        try {
+//            clientSocket = serverSocket.accept();
+//        } catch (IOException e) {
+//            System.out.println("Ошибка при подключении к: " + port);
+//            System.exit(-1);
+//        }
+//
+//        InputStream in = null;
+//        try {
+//            in = clientSocket.getInputStream();
+//        } catch (IOException e) {
+//            System.out.println("Не удалось получить поток.");
+//            System.exit(-1);
+//        }
+//
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+//        String ln = null;
+//        try {
+//            while ((ln = reader.readLine()) != null) {
+//                System.out.println(ln);
+//                System.out.flush();
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Ошибка при чтении сообщения.");
+//            System.exit(-1);
+//        }
+//    }
+//
+//    private static final int DEFAULT_PORT = 3071;
+//}
