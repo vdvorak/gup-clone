5f7d38442916340529c3cc8f9ba7a4744da74c92
Sasha <skmets@gmail.com>
2016-08-17 22:31:34 +0300

    add connect to facebook

--- oauth/src/main/java/ua/com/itproekt/gup/openid/src/main/java/com/web/SessionAttributes.java
+++
@@ -1,8 +0,0 @@
-//package com.web;
-//
-//public interface SessionAttributes {
-//
-//	public static final String ATTR_OAUTH_REQUEST_TOKEN = "oauthRequestToken";
-//	public static final String ATTR_OAUTH_ACCESS_TOKEN = "oauthAccessToken";
-//
-//}
