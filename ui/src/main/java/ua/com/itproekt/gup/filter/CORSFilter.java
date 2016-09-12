30931b62b0c400e8b7cd300aafb13d547e0e9c59
Sasha <skmets@gmail.com>
2016-09-13 02:10:21 +0300

    fix CORSFilter

--- ui/src/main/java/ua/com/itproekt/gup/filter/CORSFilter.java
+++ ui/src/main/java/ua/com/itproekt/gup/filter/CORSFilter.java
@@ -20,7 +20,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        chain.doFilter(req, res);
+        chain.doFilter(req, response); //chain.doFilter(req, res);
     }
 
     @Override
