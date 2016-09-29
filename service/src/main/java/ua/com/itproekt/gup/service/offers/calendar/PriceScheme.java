352a72db6f24a8a4d02ec8e968816313990e9526
Sasha <skmets@gmail.com>
2016-09-29 17:59:51 +0300

    add calendar controller & init-calendar

--- service/src/main/java/ua/com/itproekt/gup/service/offers/calendar/PriceScheme.java
+++ service/src/main/java/ua/com/itproekt/gup/service/offers/calendar/PriceScheme.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.Calendar;
@@ -23,34 +25,83 @@ public class PriceScheme extends ConcurrentLinkedQueue<Price> {
     @Value("${price.weekendPrice}")
     private Long weekendPrice = 12000l;
 
-    private Long[][] weekdays = {{1401570000000l,1401915600000l},{1402174800000l,1402520400000l},{1402779600000l,1403125200000l},{1403384400000l,1403730000000l},{1403989200000l,1404075600000l}}; //private Integer[][] weekdays = {{1,5},{8,12},{15,19},{22,26},{29,31}};
-    private Long[][] weekends = {{1402002000000l,1402088400000l},{1402606800000l,1402693200000l},{1403211600000l,1403298000000l},{1403816400000l,1403902800000l}}; //private Integer[][] weekends = {{6,7},{13,14},{20,21},{27,28}};
+    private boolean isInit = false;
+    private ArrayList<Long> listWeekdays = new ArrayList<Long>();
+    private ArrayList<Long> listWeekends = new ArrayList<Long>();
+    private Long[][] weekdays; //private Long[][] weekdays = {{1401570000000l,1401915600000l},{1402174800000l,1402520400000l},{1402779600000l,1403125200000l},{1403384400000l,1403730000000l},{1403989200000l,1404075600000l}}; //private Integer[][] weekdays = {{1,5},{8,12},{15,19},{22,26},{29,31}};
+    private Long[][] weekends; //private Long[][] weekends = {{1402002000000l,1402088400000l},{1402606800000l,1402693200000l},{1403211600000l,1403298000000l},{1403816400000l,1403902800000l}}; //private Integer[][] weekends = {{6,7},{13,14},{20,21},{27,28}};
 
-    public PriceScheme(){
-        init();
-    }
+    public PriceScheme(){}
 
-    public PriceScheme(Long[][] weekdays, Long[][] weekends){
-        this.weekdays = weekdays;
-        this.weekends = weekends;
-        init();
-    }
-
-    public PriceScheme(Long[][] weekdays, Long[][] weekends, Long weekdayPrice, Long weekendPrice){
-        this.weekdays = weekdays;
-        this.weekends = weekends;
-        this.weekdayPrice = weekdayPrice;
-        this.weekendPrice = weekendPrice;
-        init();
-    }
+//    public PriceScheme(Long[][] weekdays, Long[][] weekends){
+//        this.weekdays = weekdays;
+//        this.weekends = weekends;
+//        init();
+//    }
+//
+//    public PriceScheme(Long[][] weekdays, Long[][] weekends, Long weekdayPrice, Long weekendPrice){
+//        this.weekdays = weekdays;
+//        this.weekends = weekends;
+//        this.weekdayPrice = weekdayPrice;
+//        this.weekendPrice = weekendPrice;
+//        init();
+//    }
 
     public void init(){
-        addDays(weekdayPrice, weekdays);
-        addDays(weekendPrice, weekends);
+        if(!isInit){
+            addDays(weekdayPrice, weekdays);
+            addDays(weekendPrice, weekends);
+            isInit = true;
+        }
     }
 
+    public void initDate(int month, int year){
+        java.util.Calendar cal = new GregorianCalendar(year, month, 1);
+        do {
+            int day = cal.get(java.util.Calendar.DAY_OF_WEEK);
+            if (day != java.util.Calendar.SATURDAY && day != java.util.Calendar.SUNDAY) {
+                String strMonth = month<10 ? "0"+month : ""+month;
+                String strDay = cal.get(java.util.Calendar.DAY_OF_MONTH)<10 ? "0"+cal.get(java.util.Calendar.DAY_OF_MONTH) : ""+cal.get(java.util.Calendar.DAY_OF_MONTH);
+                listWeekdays.add(convertDate(strDay + "." + strMonth + "." + year));
+            }
+            cal.add(java.util.Calendar.DAY_OF_YEAR, 1);
+        }  while (cal.get(java.util.Calendar.MONTH) == month);
+
+        cal = new GregorianCalendar(year, month, 1);
+        do {
+            int day = cal.get(java.util.Calendar.DAY_OF_WEEK);
+            if (day == java.util.Calendar.SATURDAY || day == java.util.Calendar.SUNDAY) {
+                String strMonth = month<10 ? "0"+month : ""+month;
+                String strDay = cal.get(java.util.Calendar.DAY_OF_MONTH)<10 ? "0"+cal.get(java.util.Calendar.DAY_OF_MONTH) : ""+cal.get(java.util.Calendar.DAY_OF_MONTH);
+                listWeekends.add(convertDate(strDay + "." + strMonth + "." + year));
+            }
+            cal.add(java.util.Calendar.DAY_OF_YEAR, 1);
+        }  while (cal.get(java.util.Calendar.MONTH) == month);
+    }
+
+    public void initDates(Long day){
+        Integer year=Integer.valueOf(convertDate(day).split("\\.")[2]);
+        Integer month=Integer.valueOf(convertDate(day).split("\\.")[1]);
+        initDate(month, year);
+    }
+
+    public void initDates(Long[] days){
+        Arrays.sort(days);
+        for (Integer year=Integer.valueOf(convertDate(days[0]).split("\\.")[2]); year<=Integer.valueOf(convertDate(days[days.length-1]).split("\\.")[2]); ++year){
+            for (Integer month=Integer.valueOf(convertDate(days[0]).split("\\.")[1]); month<=Integer.valueOf(convertDate(days[days.length-1]).split("\\.")[1]); ++month){
+                initDate(month, year);
+            }
+        }
+    }
 
     public void addDays(Long price, Long day) {
+        initDates(day);
+        weekdays = new Long[1][listWeekdays.size()];
+        weekends = new Long[1][listWeekends.size()];
+        weekdays[0] = listWeekdays.toArray(new Long[listWeekdays.size()]);
+        weekends[0] = listWeekends.toArray(new Long[listWeekdays.size()]);
+        init();
+
         Price newPrice = new Price(price);
         for (Price curPrice : this) {
             if (curPrice.remove(day)){
@@ -61,6 +112,13 @@ public class PriceScheme extends ConcurrentLinkedQueue<Price> {
     }
 
     public void addDays(Long price, Long[] days) {
+        initDates(days);
+        weekdays = new Long[1][listWeekdays.size()];
+        weekends = new Long[1][listWeekends.size()];
+        weekdays[0] = listWeekdays.toArray(new Long[listWeekdays.size()]);
+        weekends[0] = listWeekends.toArray(new Long[listWeekdays.size()]);
+        init();
+
         switch (days.length) {
             case 1:
                 addDays(price, days[0]);
@@ -139,6 +197,20 @@ public class PriceScheme extends ConcurrentLinkedQueue<Price> {
     }
 
     /**
+     * <DAY.MONTH.YEAR> >> (Long)
+     * ("30.06.2014" >> 1472936400000)
+     *
+     * @param strDate
+     * @return
+     */
+    private Long convertDate(String strDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy", Locale.ENGLISH);
+        LocalDate localDate = LocalDate.parse(strDate, formatter);
+        Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        return date.getTime();
+    }
+
+    /**
      * (Long) >> <DAY.MONTH.YEAR>
      * 1472936400000 >> "30.06.2014"
      *
@@ -149,5 +221,17 @@ public class PriceScheme extends ConcurrentLinkedQueue<Price> {
         Date date = new Date(lDate);
         String strDate = simpleDateFormat.format(date);
         return strDate;
+    }
+
+    /**
+     * @param strDate
+     * @return
+     */
+    private Long[] convertDate(String[] strDate) {
+        Long[] longDate = new Long[strDate.length];
+        for (int date=0; date<strDate.length; ++date){
+            longDate[date] = convertDate(strDate[date]);
+        }
+        return longDate;
     }
 }
--- service/src/test/java/ua/com/itproekt/gup/service/offers/calendar/PriceSchemeTest.java
+++ service/src/test/java/ua/com/itproekt/gup/service/offers/calendar/PriceSchemeTest.java
@@ -38,69 +38,64 @@ public class PriceSchemeTest {
     public void tearDown() {
     }
 
-    public void printDays(int year, int month){
-        System.out.print("\nweekdays: ");
+    private ArrayList<Long> listWeekdays = new ArrayList<Long>();
+    private ArrayList<Long> listWeekends = new ArrayList<Long>();
+
+
+    public void initDate(int month, int year){
         java.util.Calendar cal = new GregorianCalendar(year, month, 1);
         do {
             int day = cal.get(java.util.Calendar.DAY_OF_WEEK);
             if (day != java.util.Calendar.SATURDAY && day != java.util.Calendar.SUNDAY) {
-                System.out.print(cal.get(java.util.Calendar.DAY_OF_MONTH) + "." + month + "." + year +  " ");
+                String strMonth = month<10 ? "0"+month : ""+month;
+                String strDay = cal.get(java.util.Calendar.DAY_OF_MONTH)<10 ? "0"+cal.get(java.util.Calendar.DAY_OF_MONTH) : ""+cal.get(java.util.Calendar.DAY_OF_MONTH);
+                listWeekdays.add(convertDate(strDay + "." + strMonth + "." + year));
             }
             cal.add(java.util.Calendar.DAY_OF_YEAR, 1);
         }  while (cal.get(java.util.Calendar.MONTH) == month);
 
-        System.out.println();
-        System.out.print("weekends: ");
         cal = new GregorianCalendar(year, month, 1);
         do {
             int day = cal.get(java.util.Calendar.DAY_OF_WEEK);
             if (day == java.util.Calendar.SATURDAY || day == java.util.Calendar.SUNDAY) {
-                System.out.print(cal.get(java.util.Calendar.DAY_OF_MONTH) + "." + month + "." + year +  " ");
+                String strMonth = month<10 ? "0"+month : ""+month;
+                String strDay = cal.get(java.util.Calendar.DAY_OF_MONTH)<10 ? "0"+cal.get(java.util.Calendar.DAY_OF_MONTH) : ""+cal.get(java.util.Calendar.DAY_OF_MONTH);
+                listWeekends.add(convertDate(strDay + "." + strMonth + "." + year));
             }
             cal.add(java.util.Calendar.DAY_OF_YEAR, 1);
         }  while (cal.get(java.util.Calendar.MONTH) == month);
     }
 
-    public void printDays(Long[] aa){
-        Arrays.sort(aa);
-        for (Integer year=Integer.valueOf(convertDate(aa[0]).split("\\.")[2]); year<=Integer.valueOf(convertDate(aa[aa.length-1]).split("\\.")[2]); ++year){
-            for (Integer month=Integer.valueOf(convertDate(aa[0]).split("\\.")[1]); month<=Integer.valueOf(convertDate(aa[aa.length-1]).split("\\.")[1]); ++month){
-                printDays(year, month);
+    public void initDates(Long[] dates){
+        Arrays.sort(dates);
+        for (Integer year=Integer.valueOf(convertDate(dates[0]).split("\\.")[2]); year<=Integer.valueOf(convertDate(dates[dates.length-1]).split("\\.")[2]); ++year){
+            for (Integer month=Integer.valueOf(convertDate(dates[0]).split("\\.")[1]); month<=Integer.valueOf(convertDate(dates[dates.length-1]).split("\\.")[1]); ++month){
+                initDate(month, year);
             }
         }
     }
 
-    /**
-     * Test(s) Output-Days
-     */
     @Test
     public void testOutDays() {
-        /////////////////////////////////////////////////////////////////////////
-        Long aa[] = {1401570000000l,1401915600000l,1402174800000l,1402520400000l,1402779600000l,1403125200000l,1403384400000l,1403730000000l,1403989200000l,1404075600000l,1402002000000l,1402088400000l,1402606800000l,1402693200000l,1403211600000l,1403298000000l,1403816400000l,1403902800000l};
-//        for (Long a : aa) System.out.print(convertDate(a) + " ");
-//
-//        System.out.println();
-//        Arrays.sort(aa);
-//        for (Long a : aa) System.out.print(convertDate(a) + " ");
-//
-//        System.out.println();
-//        System.out.println("(min)" + convertDate(aa[0]) + " (max)" + convertDate(aa[aa.length-1]) + "");
-//        System.out.println("(start-month)" + Integer.valueOf(convertDate(aa[0]).split("\\.")[1]) + " (last-month)" + Integer.valueOf(convertDate(aa[aa.length-1]).split("\\.")[1]) + "");
-//        System.out.println("(start-year)" + Integer.valueOf(convertDate(aa[0]).split("\\.")[2]) + " (last-year)" + Integer.valueOf(convertDate(aa[aa.length-1]).split("\\.")[2]) + "");
+        String[] offerCalendar1 = {"22.06.2014","31.06.2014"};
+        initDates(convertDate(offerCalendar1));
 
+        System.out.println("weekdays: " + listWeekdays);
+        System.out.println("weekends: " + listWeekends);
+    }
 
-        /////////////////////////////////////////////////////////////////////////
-        printDays(aa);
-
-        /////////////////////////////////////////////////////////////////////////
-//        System.out.println(TestDays.valueOf("06.06.2014"));
-        System.out.println(TestDays.ONE);
-
-
-//        System.out.println("START:");
-//        printWeekdays(Integer.valueOf(convertDate(aa[0]).split("\\.")[2]), Integer.valueOf(convertDate(aa[0]).split("\\.")[1]));
-//        System.out.println("\nLAST:");
-//        printWeekdays(Integer.valueOf(convertDate(aa[aa.length-1]).split("\\.")[2]), Integer.valueOf(convertDate(aa[aa.length-1]).split("\\.")[1]));
+    /**
+     * <DAY.MONTH.YEAR> >> (Long)
+     * ("30.06.2014" >> 1472936400000)
+     *
+     * @param strDate
+     * @return
+     */
+    private Long convertDate(String strDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy", Locale.ENGLISH);
+        LocalDate localDate = LocalDate.parse(strDate, formatter);
+        Date date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        return date.getTime();
     }
 
     /**
@@ -114,6 +109,18 @@ public class PriceSchemeTest {
         Date date = new Date(lDate);
         String strDate = simpleDateFormat.format(date);
         return strDate;
+    }
+
+    /**
+     * @param strDate
+     * @return
+     */
+    private Long[] convertDate(String[] strDate) {
+        Long[] longDate = new Long[strDate.length];
+        for (int date=0; date<strDate.length; ++date){
+            longDate[date] = convertDate(strDate[date]);
+        }
+        return longDate;
     }
 
 }
