7a16c335b14bcaf8274ad9dadccc1b7d62ffe21b
Optionix <alexdubitandum@gmail.com>
2016-10-11 13:04:13 +0300

    Ahtung! Alarma! Admin doesn't work!

--- admin/src/main/java/ua/com/itproekt/gup/controller/AdminController.java
+++ admin/src/main/java/ua/com/itproekt/gup/controller/AdminController.java
@@ -1,43 +1,38 @@
 package ua.com.itproekt.gup.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ua.com.itproekt.gup.service.profile.ProfilesService;
+
 
 @Controller
 public class AdminController {
 
-    @Autowired
-    ProfilesService profilesService;
 
-
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    //    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping("/admin")
     public String getLoginForm() {
         return "admin/admin-main";
     }
 
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    //    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping("/admin-users")
     public String getAdminUsers() {
         return "admin/admin-users";
     }
 
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    //    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping("/admin-offers")
     public String getAdminOffers() {
         return "admin/admin-offers";
     }
 
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    //    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping("/admin-tariffs")
     public String getAdminTariffs() {
         return "admin/admin-tariffs";
     }
 
-//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    //    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping("/admin-edit-tariffs")
     public String editAdminTariffs() {
         return "admin/admin-edit-tariffs";
