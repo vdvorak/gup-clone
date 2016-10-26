7ba99f03a25f163003883bd9526f73b7e9fe4498
Optionix <alexdubitandum@gmail.com>
2016-10-26 14:00:42 +0300

    First ever mongodb junit test in da Univers

--- core/src/main/test/java/ua/com/itproekt/gup/dao/OfferRepositoryTest.java
+++ core/src/main/test/java/ua/com/itproekt/gup/dao/OfferRepositoryTest.java
@@ -1,8 +1,5 @@
 package ua.com.itproekt.gup.dao;
 
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.fakemongo.Fongo;
 import com.mongodb.Mongo;
 import org.junit.After;
@@ -11,23 +8,36 @@ import org.junit.Test;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import ua.com.itproekt.gup.dao.offers.OfferRepositoryImpl;
 import ua.com.itproekt.gup.model.offer.Offer;
-import ua.com.itproekt.gup.model.offer.filter.OfferFilterOptions;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
 
 public class OfferRepositoryTest {
 
     OfferRepositoryImpl offerRepository = new OfferRepositoryImpl();
     MongoTemplate mongoTemplate;
     Mongo mongo;
-    Fongo fongo = new Fongo("Test");
+    Fongo fongo = new Fongo("TestOffer");
+
+    File oneOfferFile = new File("src/main/test/resources/", "OffersMongoSeeds.json");
 
     @Before
     public void setup() {
 
+        // create fake mongo database
         mongo = fongo.getMongo();
         mongoTemplate = new MongoTemplate(mongo, "dbName");
         fongo.dropDatabase("dbName");
 
         offerRepository.setMongoTemplate(mongoTemplate);
+
+//        System.err.println("Saving to the file");
+        mongoTemplate.insert(oneOfferFile, "offer");
+
+//        System.err.println("All Collections: " + mongoTemplate.getCollectionNames());
+//        System.err.println("All documents in offer collection: " + mongoTemplate.findAll(Offer.class, "offer"));
+
     }
 
 
@@ -37,21 +47,19 @@ public class OfferRepositoryTest {
 
 
     @Test
-    public void test_in_memory_mongodb() {
+    public void create_oneDocument_shouldCreateOneDocument() {
 
+        //given
+        mongoTemplate.insert(oneOfferFile, "offer");
+        Offer offer = mongoTemplate.findAll(Offer.class, "offer").get(0);
+        String actualId = offer.getId();
 
-        OfferFilterOptions offerFilterOptions = new OfferFilterOptions();
-        offerFilterOptions.setSkip(0);
-        offerFilterOptions.setLimit(10);
+        //when
+        Offer foundOffer = offerRepository.findById(actualId);
+        String expectedId = foundOffer.getId();
 
-        Offer offer = new Offer();
-        offer.setActive(true);
-
-        offerRepository.create(offer);
-
-        System.err.println("test" + offerRepository.findOffersWihOptions(offerFilterOptions));
-
-        System.err.println("test2" + mongoTemplate.toString());
+        //then
+        assertEquals(expectedId, actualId);
     }
 
 
