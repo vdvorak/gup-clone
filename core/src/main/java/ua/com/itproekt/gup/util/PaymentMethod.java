4c78851676bfa78d8dde9740bc7889f8fc2ccc37
Sasha <skmets@gmail.com>
2017-02-08 14:04:45 +0200

    (#371) курьеру, либо при самовывозе -> это наличка

--- core/src/main/java/ua/com/itproekt/gup/util/PaymentMethod.java
+++ core/src/main/java/ua/com/itproekt/gup/util/PaymentMethod.java
@@ -10,6 +10,7 @@ public enum PaymentMethod {
     GUP, // безопасная сделка через ГУП
     CASH_ON_DELIVERY, // наложенный платёж
     CARD_PAYMENT, // предоплата
-    CASH_PAYMENT, //COURIER, // курьеру, либо при самовывозе
+    COURIER, // курьеру, либо при самовывозе & нал
+    CASH_PAYMENT, // only нал.
     CASH_LESS_PAYMENT // безнал
 }
