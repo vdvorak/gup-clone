58186edfacd74af8b34e2bb81a6709a08f21cddd
Sasha <skmets@gmail.com>
2017-02-17 19:42:51 +0200

    add REST method(s) to Buy By Bonus Account add integration test(s) to Buy By Bonus Account on REST-Service

--- ui/src/main/java/ua/com/itproekt/gup/server/api/rest/invitecodes/InviteCodeRestController.java
+++ ui/src/main/java/ua/com/itproekt/gup/server/api/rest/invitecodes/InviteCodeRestController.java
@@ -5,12 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import ua.com.itproekt.gup.bank_api.BankSession;
+import ua.com.itproekt.gup.bank_api.entity.InternalTransaction;
 import ua.com.itproekt.gup.util.SecurityOperations;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -20,6 +21,14 @@ public class InviteCodeRestController {
     @Autowired
     private BankSession bankSession;
 
+    /**
+     * Method add exchanges invite code on bonus amount for user
+     *
+     * FUNCTION 'fn_check_bonus_amount(<current_user_id>, <current_code>)'
+     *
+     * @param inviteCode
+     * @return
+     */
     @CrossOrigin
     @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/code/exchange", method = RequestMethod.POST)
@@ -29,5 +38,66 @@ public class InviteCodeRestController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+
+
+    /**
+     * Method return user bonus balance amount.
+     *
+     * FUNCTION 'fn_check_bonus_amount(<current_user_id>)'
+     *
+     * @return
+     */
+    @CrossOrigin
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/code/exchange", method = RequestMethod.GET)
+    public ResponseEntity<String> inviteCodeExchange() {
+        String userId = SecurityOperations.getLoggedUserId();
+        String bonus = bankSession.getBonusByUserId(userId);
+        return new ResponseEntity<>(bonus, HttpStatus.OK);
+    }
+
+    /**
+     * Method allow to buy paid services for bonus money.
+     *
+     * FUNCTION 'fn_buy_bonus_account(<current_user_id>, <current_trans_type>, <current_amount>, <current_offer_id>)'
+     *
+     * @param transType
+     * @param cost
+     * @param offerId
+     * @return
+     */
+    @CrossOrigin
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/bonus/buy", method = RequestMethod.POST)
+    public ResponseEntity<String> buyByBonusAccount(
+            @RequestParam("transType") String transType,
+            @RequestParam("cost") String cost,
+            @RequestParam("offerId") String offerId){
+        String userId = SecurityOperations.getLoggedUserId();
+        String buy = bankSession.buyByBonusAccount(userId, Integer.valueOf(transType), Integer.valueOf(cost), offerId);
+
+        return new ResponseEntity<>(buy, HttpStatus.OK);
+    }
+
+    /**
+     * Method return all internal transactions which satisfied input parameters.
+     *
+     * FUNCTION 'fn_check_internal_transactions(<current_user_id>, <by_type>)'
+     *
+     * @param transType
+     * @return
+     * @throws Exception
+     */
+    @CrossOrigin
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/bonus/buy", method = RequestMethod.GET)
+    public ResponseEntity<String> buyByBonusAccount(
+            @RequestParam("transType") String transType)
+            throws Exception {
+        String userId = SecurityOperations.getLoggedUserId();
+        String res = bankSession.getAllPendingTransactions(userId, transType);
+
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
 
 }
