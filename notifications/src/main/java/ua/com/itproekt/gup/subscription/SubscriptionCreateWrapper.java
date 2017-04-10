d2fb845e2055227c3bc05a78a46577fa7b0ca3d9
Sasha <skmets@gmail.com>
2017-04-10 18:26:07 +0300

    to subscribe controller

---
+++ notifications/src/main/java/ua/com/itproekt/gup/subscription/SubscriptionCreateWrapper.java
@@ -0,0 +1,38 @@
+package ua.com.itproekt.gup.subscription;
+
+import ua.com.itproekt.gup.model.offer.filter.OfferFilterOptions;
+
+/**
+ * Wrapper class for request body for create subscription.
+ *
+ * @author Kobylyatskyy Alexander
+ */
+public class SubscriptionCreateWrapper {
+
+    private OfferFilterOptions offerFilterOptions;
+    private String email; // email for not authorized person
+
+    public OfferFilterOptions getOfferFilterOptions() {
+        return offerFilterOptions;
+    }
+
+    public void setOfferFilterOptions(OfferFilterOptions offerFilterOptions) {
+        this.offerFilterOptions = offerFilterOptions;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "SubscriptionCreateWrapper{" +
+                "offerFilterOptions=" + offerFilterOptions +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
