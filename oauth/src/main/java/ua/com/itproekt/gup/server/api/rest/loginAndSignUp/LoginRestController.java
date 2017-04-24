8d7542cc1fa623344d2baceaa05c55b604069851
Sasha <skmets@gmail.com>
2017-04-24 17:21:17 +0300

    set timeout use last try enter of password

--- oauth/src/main/java/ua/com/itproekt/gup/server/api/rest/loginAndSignUp/LoginRestController.java
+++ oauth/src/main/java/ua/com/itproekt/gup/server/api/rest/loginAndSignUp/LoginRestController.java
@@ -356,9 +356,9 @@ public class LoginRestController {
 //            System.out.println("Email=" + formLoggedUser.getEmail() + " Password=" + formLoggedUser.getPassword());
 //            System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 
-            if(profilesService.findPrivateProfileByEmailAndUpdateLastTryLoginDate(formLoggedUser.getEmail())){
-                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-            }
+//            if(profilesService.findPrivateProfileByEmailAndUpdateLastTryLoginDate(formLoggedUser.getEmail())){
+//                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+//            }
 
             loggedUser = (LoggedUser) userDetailsService.loadUserByUsername(formLoggedUser.getEmail());
         } catch (UsernameNotFoundException ex) {
