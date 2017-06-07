ddbdb0237c9a6b26966bba34c9c8d9ab4e833cc7
Sasha <skmets@gmail.com>
2017-06-07 14:09:45 +0300

    issues/887

--- service/src/main/java/ua/com/gup/service/dto/offer/view/OfferViewShortDTO.java
+++ service/src/main/java/ua/com/gup/service/dto/offer/view/OfferViewShortDTO.java
@@ -1,6 +1,7 @@
 package ua.com.gup.service.dto.offer.view;
 
 import io.swagger.annotations.ApiModelProperty;
+import ua.com.gup.domain.offer.OfferStatistic;
 import ua.com.gup.service.dto.offer.OfferAddressShortDTO;
 
 public class OfferViewShortDTO extends OfferViewBaseDTO {
@@ -8,11 +9,22 @@ public class OfferViewShortDTO extends OfferViewBaseDTO {
     @ApiModelProperty(position = 60)
     private OfferAddressShortDTO address;
 
+    @ApiModelProperty(position = 140)
+    private OfferStatistic statistic;
+
     public OfferAddressShortDTO getAddress() {
         return address;
     }
 
     public void setAddress(OfferAddressShortDTO address) {
         this.address = address;
+    }
+
+    public OfferStatistic getStatistic() {
+        return statistic;
+    }
+
+    public void setStatistic(OfferStatistic statistic) {
+        this.statistic = statistic;
     }
 }
