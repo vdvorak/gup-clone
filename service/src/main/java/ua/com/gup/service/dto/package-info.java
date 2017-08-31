f6cd2815b0055f9add82f1d7f243e89f5a34ed86
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-08-31 16:31:14 +0300

    hot fix refactoring

--- service/src/main/java/ua/com/gup/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package ua.com.gup.service.dto;
--- service/src/main/java/ua/com/gup/service/mapper/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * MapStruct mappers for mapping domain objects and Data Transfer Objects.
- */
-package ua.com.gup.service.mapper;
