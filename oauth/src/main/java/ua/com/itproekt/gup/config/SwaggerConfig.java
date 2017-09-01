d8ab011f98f4027ff8dfc0ea91d4c346ddae77bd
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-09-01 11:05:01 +0300

    hot fix

--- oauth/src/main/java/ua/com/itproekt/gup/config/SwaggerConfig.java
+++ oauth/src/main/java/ua/com/itproekt/gup/config/SwaggerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,15 +13,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-//    public Docket apiItproekt() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .groupName("oauth")
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("ua.com.itproekt.gup"))
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(apiInfo());
-//    }
     public Docket apiOauth() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("oauth")
