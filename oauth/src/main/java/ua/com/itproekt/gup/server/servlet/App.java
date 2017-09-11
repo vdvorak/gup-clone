3fe671a87d4e009f1a451e84a35e27bdf38ded71
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-09-11 17:45:10 +0300

    re-factoring and hot fix loogger

--- oauth/src/main/java/ua/com/itproekt/gup/server/servlet/App.java
+++ oauth/src/main/java/ua/com/itproekt/gup/server/servlet/App.java
@@ -1,13 +1,13 @@
 package ua.com.itproekt.gup.server.servlet;
 
-import java.net.URL;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-
 import org.apache.log4j.Logger;
 import org.apache.log4j.helpers.Loader;
 import org.apache.log4j.xml.DOMConfigurator;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import java.net.URL;
 
 public class App extends HttpServlet {
 	private static final long  serialVersionUID = 199465984923187558L;
@@ -15,11 +15,9 @@ public class App extends HttpServlet {
 	private static Logger                   log = Logger.getLogger(App.class);
 
 	public void init(ServletConfig config) throws ServletException {
-		// load the log4j config
 		URL url = Loader.getResource(LOG4J_CONFIG_FILE_XML);
 		DOMConfigurator.configure(url);
-		log.debug("Initialized log4j");
-
+		log.info("Initialized log4j");
 		super.init(config);
 	}
 }
