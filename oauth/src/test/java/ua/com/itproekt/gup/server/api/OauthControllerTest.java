4de8e384a32e612c0156b72f73b59174f21ab024
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-09-14 19:21:03 +0300

    move in test directory

--- oauth/src/test/java/ua/com/itproekt/gup/server/api/OauthControllerTest.java
+++ oauth/src/test/java/ua/com/itproekt/gup/server/api/OauthControllerTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath*:applicationContext.xml"})
+@ContextConfiguration({"/appContext.xml"})
 @WebAppConfiguration
 public class OauthControllerTest {
 
