82eba955adcd0d7fbe8cdb84a0bbaf0b04d41090
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-09-25 10:50:04 +0300

    re-factoring code

--- service/src/main/java/ua/com/gup/config/SchedulerConfig.java
+++ service/src/main/java/ua/com/gup/config/SchedulerConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import java.util.concurrent.Executor;
