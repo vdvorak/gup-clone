f0f0d0d9b309310dc00949286b3f147534c2d836
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-09-25 13:06:16 +0300

    import remove not use

--- ui/src/main/java/ua/com/gup/web/rest/api/geo/GeoController.java
+++ ui/src/main/java/ua/com/gup/web/rest/api/geo/GeoController.java
@@ -4,7 +4,6 @@ package ua.com.gup.web.rest.api.geo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import ua.com.gup.model.geo.GeoModel;
