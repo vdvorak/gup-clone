7d68a54cf6a9ab6c41105bc630c29d4d85f602cd
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-10-03 22:03:46 +0300

    re-factoring

--- service/src/main/java/ua/com/gup/service/offer/OfferService.java
+++ service/src/main/java/ua/com/gup/service/offer/OfferService.java
@@ -141,7 +141,7 @@ public interface OfferService {
      * @return true if an user has permission for update, {@literal false} otherwise
      * @throws IllegalArgumentException if {@code id} is {@literal null}
      */
-    boolean hasPermissionForUpdate(String id);
+    boolean hasPermissionForUpdate(String offerId);
 
     /**
      * Update active offers base price by current exchange rate.
