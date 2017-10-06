3f475bfdeaa4b61a1801bdc12bb6b3a8ef8a404f
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-10-06 18:15:17 +0300

    https://redmine.dev.gup.ua/redmine/issues/1249

--- service/src/main/java/ua/com/gup/mapper/AddressMapper.java
+++ service/src/main/java/ua/com/gup/mapper/AddressMapper.java
@@ -12,16 +12,17 @@ public class AddressMapper {
     public OfferAddressDTO addressToAddressDTO(Address address) {
         OfferAddressDTO offerAddressDTO = new OfferAddressDTO();
         if (address != null) {
-            if (address.getLocation()!=null) {
+            if (address.getLocation() != null) {
                 offerAddressDTO.setLat(address.getLocation().getY());
                 offerAddressDTO.setLng(address.getLocation().getX());
             }
+            offerAddressDTO.setLevel(address.getLevel());
             offerAddressDTO.setCountry(address.getCountry());
+            offerAddressDTO.setRegion(address.getRegion());
             offerAddressDTO.setDistrict(address.getDistrict());
+            offerAddressDTO.setCouncil(address.getCouncil());
             offerAddressDTO.setCity(address.getCity());
             offerAddressDTO.setFullAddress(address.getFullAddress());
-            address.setLevel(offerAddressDTO.getLevel());
-
         }
         return offerAddressDTO;
     }
@@ -29,7 +30,7 @@ public class AddressMapper {
     public OfferAddressShortDTO addressToAddressShortDTO(Address address) {
         OfferAddressShortDTO offerAddressShortDTO = new OfferAddressShortDTO();
         if (address != null) {
-            if(address.getLocation()!=null) {
+            if (address.getLocation() != null) {
                 offerAddressShortDTO.setLat(address.getLocation().getX());
                 offerAddressShortDTO.setLng(address.getLocation().getY());
             }
@@ -39,12 +40,16 @@ public class AddressMapper {
 
     public Address addressDTOToAddress(OfferAddressDTO offerAddressDTO) {
         Address address = new Address();
-        address.setLocation(new GeoJsonPoint(offerAddressDTO.getLng(), offerAddressDTO.getLat()));
-        address.setCountry(offerAddressDTO.getCountry());
-        address.setDistrict(offerAddressDTO.getDistrict());
-        address.setCity(offerAddressDTO.getCity());
-        address.setFullAddress(offerAddressDTO.getFullAddress());
-        address.setLevel(offerAddressDTO.getLevel());
+        if (offerAddressDTO != null) {
+            address.setLocation(new GeoJsonPoint(offerAddressDTO.getLng(), offerAddressDTO.getLat()));
+            address.setLevel(offerAddressDTO.getLevel());
+            address.setCountry(offerAddressDTO.getCountry());
+            address.setRegion(offerAddressDTO.getRegion());
+            address.setDistrict(offerAddressDTO.getDistrict());
+            address.setCouncil(offerAddressDTO.getCouncil());
+            address.setCity(offerAddressDTO.getCity());
+            address.setFullAddress(offerAddressDTO.getFullAddress());
+        }
         return address;
     }
 }
