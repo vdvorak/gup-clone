b3b7f3b748ea8aee622d3f64b6f5bc1c0f4b3dd0
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-10-06 17:43:08 +0300

    Scheduled - stop

--- service/src/main/java/ua/com/gup/executor/mail/SendMailTask.java
+++ service/src/main/java/ua/com/gup/executor/mail/SendMailTask.java
@@ -3,8 +3,6 @@ package ua.com.gup.executor.mail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import ua.com.gup.domain.email.EmailMessage;
 import ua.com.gup.service.emailnotification.EmailService;
@@ -18,7 +16,7 @@ public class SendMailTask {
     @Autowired
     private EmailService emailService;
 
-    @Scheduled(cron = "* * * * * *")
+    //@Scheduled(cron = "* * * * * *")
     public void printMessages() {
         EmailMessage message = emailService.findOneMessage();
         if (message != null) {
