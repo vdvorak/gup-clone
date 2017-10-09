1691cc718c0bc1a305e394b9341072dfca43f094
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-10-09 17:40:29 +0300

    re-factoring code

--- core/src/main/java/ua/com/gup/repository/offer/OfferRepository.java
+++ core/src/main/java/ua/com/gup/repository/offer/OfferRepository.java
@@ -3,23 +3,20 @@ package ua.com.gup.repository.offer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import ua.com.gup.domain.offer.Offer;
 import ua.com.gup.domain.enumeration.OfferStatus;
+import ua.com.gup.domain.offer.Offer;
 
 import java.util.Optional;
 
 /**
  * Repository for the offer entity.
  */
-public interface OfferRepository extends MongoRepository<Offer, String>, OfferRepositoryCustom {
-
+public interface OfferRepository extends MongoRepository<Offer, String> {
 
     Optional<Offer> findOneBySeoUrl(String seoUrl);
-
     Page<Offer> findAllByStatusAndAuthorId(OfferStatus status, String authorId, Pageable pageable);
-
     Page<Offer> findAllByStatus(OfferStatus status, Pageable pageable);
 
-    Offer findOfferByIdAndAuthorId(String offerId,String authorId);
+    Optional<Offer> findOfferByIdAndAuthorId(String offerId, String authorId);
 
 }
