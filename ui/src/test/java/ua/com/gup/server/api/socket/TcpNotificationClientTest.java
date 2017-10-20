b2a9476b480d5819d3f34842bbf67dc5e7f672bc
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-10-20 17:25:47 +0300

    re-factoring library and hot fix

--- ui/src/test/java/ua/com/gup/server/api/socket/TcpNotificationClientTest.java
+++ ui/src/test/java/ua/com/gup/server/api/socket/TcpNotificationClientTest.java
@@ -1,56 +1,59 @@
-//package ua.com.itproekt.gup.socket;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.InputStreamReader;
-//import java.net.ServerSocket;
-//import java.net.Socket;
-//
-///**
-//* Слушает указанный в первом параметре порт и выводит все приходящие на него сообщения на консоль.
-//*/
-//public class TcpNotificationClientTest {
-//
-//    public static void main(String[] args) {
-//        int port = DEFAULT_PORT;
-//
-//        ServerSocket serverSocket = null;
-//        try {
-//            serverSocket = new ServerSocket(port);
-//        } catch (IOException e) {
-//            System.out.println("занят: " + port);
-//            System.exit(-1);
-//        }
-//
-//        Socket clientSocket = null;
-//        try {
-//            clientSocket = serverSocket.accept();
-//        } catch (IOException e) {
-//            System.out.println("Ошибка при подключении к: " + port);
-//            System.exit(-1);
-//        }
-//
-//        InputStream in = null;
-//        try {
-//            in = clientSocket.getInputStream();
-//        } catch (IOException e) {
-//            System.out.println("Не удалось получить поток.");
-//            System.exit(-1);
-//        }
-//
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-//        String ln = null;
-//        try {
-//            while ((ln = reader.readLine()) != null) {
-//                System.out.println(ln);
-//                System.out.flush();
-//            }
-//        } catch (IOException e) {
-//            System.out.println("Ошибка при чтении сообщения.");
-//            System.exit(-1);
-//        }
-//    }
-//
-//    private static final int DEFAULT_PORT = 3071;
-//}
+package ua.com.gup.server.api.socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+* Слушает указанный в первом параметре порт и выводит все приходящие на него сообщения на консоль.
+*/
+public class TcpNotificationClientTest {
+    private final static Logger log = LoggerFactory.getLogger(TcpNotificationClientTest.class);
+    private static final int DEFAULT_PORT = 3071;
+
+    public static void main(String[] args) {
+        int port = DEFAULT_PORT;
+
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            log.info("занят: {} ",  port);
+            System.exit(-1);
+        }
+
+        Socket clientSocket = null;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            log.info("Ошибка при подключении к: {} " , port);
+            System.exit(-1);
+        }
+
+        InputStream in = null;
+        try {
+            in = clientSocket.getInputStream();
+        } catch (IOException e) {
+            log.info("Не удалось получить поток.");
+            System.exit(-1);
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String ln = null;
+        try {
+            while ((ln = reader.readLine()) != null) {
+                log.info("{}",ln);
+            }
+        } catch (IOException e) {
+            log.info("Ошибка при чтении сообщения.");
+            System.exit(-1);
+        }
+    }
+
+}
