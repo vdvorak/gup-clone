7910b9c320410ec92e9045de6c770e3046e81875
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-10-27 21:43:18 +0300

    add Client Details table for spring-security-oauth2 and Repository annd Service Layer

--- repository/src/main/java/ua/com/gup/repository/client/ClientDetailsRepository.java
+++ repository/src/main/java/ua/com/gup/repository/client/ClientDetailsRepository.java
@@ -3,13 +3,11 @@ package ua.com.gup.repository.client;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import ua.com.gup.mongo.composition.domain.client.ClientDetail;
 
-import java.util.Optional;
-
 /**
  * @author Victor Dvorak
  * Repository for the oauth2_client  entity.
  **/
 
-public interface ClientDetailRepository extends MongoRepository<ClientDetail, String> {
-     Optional<ClientDetail> findByClientId(String clientId);
+public interface ClientDetailsRepository extends MongoRepository<ClientDetail, String> {
+     ClientDetail findByClientId(String clientId);
 }
