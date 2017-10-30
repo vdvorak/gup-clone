eacf43027ed9d698557ec434b21bb341eed5dcfc
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-10-30 13:54:58 +0200

    modify security from xml to java bean config

--- oauth/src/main/java/ua/com/gup/OAuthGupApplication.java
+++ oauth/src/main/java/ua/com/gup/OAuthGupApplication.java
@@ -2,7 +2,11 @@ package ua.com.gup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import ua.com.gup.config.*;
+import ua.com.gup.config.MailConfig;
+import ua.com.gup.config.MongoConfig;
+import ua.com.gup.config.RootConfig;
+import ua.com.gup.config.WebConfig;
+import ua.com.gup.config.security.SecurityConfig;
 import ua.com.gup.config.swagger.SwaggerConfig;
 
 @EnableAutoConfiguration
