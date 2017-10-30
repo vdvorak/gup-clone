90192ade5e144e12fe6dc376bbffc8c9d3dbe91b
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-10-30 13:56:35 +0200

    modify security from xml to java bean config

--- oauth/src/main/java/ua/com/gup/config/security/SecurityConfig.java
+++ oauth/src/main/java/ua/com/gup/config/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement().maximumSessions(2);
+        http.sessionManagement().maximumSessions(1);
     }
 
     @Override
