339bc5bdf1ddda1d8ae5bf4730f46b457261a57d
ivalex <al.ivashkov@gmail.com>
2017-11-07 13:08:09 +0200

    remove auto fill index

--- search/src/main/java/ua/com/gup/search/FullTextSearchApplication.java
+++ search/src/main/java/ua/com/gup/search/FullTextSearchApplication.java
@@ -1,21 +1,16 @@
 package ua.com.gup.search;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ua.com.gup.search.config.CLApp;
 import ua.com.gup.search.config.RootConfig;
 import ua.com.gup.search.config.WebConfig;
-import ua.com.gup.search.repository.ESCategoryRepository;
-import ua.com.gup.search.repository.ESOfferRepository;
 
 @SpringBootApplication
 public class FullTextSearchApplication {
 
 
-
     public static void main(String[] args) {
-        SpringApplication.run(new Object[]{RootConfig.class, WebConfig.class, FullTextSearchApplication.class, CLApp.class}, args);
+        SpringApplication.run(new Object[]{RootConfig.class, WebConfig.class, FullTextSearchApplication.class}, args);
 
 
     }
