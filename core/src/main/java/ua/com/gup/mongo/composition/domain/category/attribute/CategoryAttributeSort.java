1eb4e445e78f0607ee0e0009a665b1611f1785f3
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-11-07 14:14:21 +0200

    add table for sort category_attribute and acategory

---
+++ core/src/main/java/ua/com/gup/mongo/composition/domain/category/attribute/CategoryAttributeSort.java
@@ -0,0 +1,147 @@
+package ua.com.gup.mongo.composition.domain.category.attribute;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import ua.com.gup.mongo.model.category.attribute.CategoryAttributeValue;
+import ua.com.gup.mongo.model.category.attribute.validator.CategoryAttributeValidator;
+import ua.com.gup.mongo.model.enumeration.CategoryAttributeType;
+
+import java.io.Serializable;
+import java.util.*;
+
+@Document(collection = CategoryAttributeSort.COLLECTION_NAME)
+public class CategoryAttributeSort implements Serializable {
+
+    public static final String COLLECTION_NAME = "category_attribute";
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    private int code;
+
+    private boolean active;
+
+    private String key;
+
+    private Map<String, String> title = new HashMap<>();
+
+    private Map<String, String> unit = new HashMap<>();
+
+    private Set<Integer> categories = new HashSet<>();
+
+    private CategoryAttributeType type;
+
+    private CategoryAttributeValidator validator;
+
+    private LinkedHashSet<CategoryAttributeValue> values = new LinkedHashSet<>();
+
+    private boolean privateAttr;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Map<String, String> getTitle() {
+        return title;
+    }
+
+    public void setTitle(Map<String, String> title) {
+        this.title = title;
+    }
+
+    public Map<String, String> getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Map<String, String> unit) {
+        this.unit = unit;
+    }
+
+    public Set<Integer> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Integer> categories) {
+        this.categories = categories;
+    }
+
+    public CategoryAttributeType getType() {
+        return type;
+    }
+
+    public void setType(CategoryAttributeType type) {
+        this.type = type;
+    }
+
+    public CategoryAttributeValidator getValidator() {
+        return validator;
+    }
+
+    public void setValidator(CategoryAttributeValidator validator) {
+        this.validator = validator;
+    }
+
+    public LinkedHashSet<CategoryAttributeValue> getValues() {
+        return values;
+    }
+
+    public void setValues(LinkedHashSet<CategoryAttributeValue> values) {
+        this.values = values;
+    }
+
+    public boolean isPrivateAttr() {
+        return privateAttr;
+    }
+
+    public void setPrivateAttr(boolean privateAttr) {
+        this.privateAttr = privateAttr;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CategoryAttributeSort that = (CategoryAttributeSort) o;
+
+        if (code != that.code) return false;
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + code;
+        return result;
+    }
+}
