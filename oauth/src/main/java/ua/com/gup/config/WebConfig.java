d50146b18bc412bcb886c3ad4acc6a9afc9594fd
ivalex <al.ivashkov@gmail.com>
2017-11-09 12:40:14 +0200

    cleanup

--- oauth/src/main/java/ua/com/gup/config/WebConfig.java
+++ oauth/src/main/java/ua/com/gup/config/WebConfig.java
@@ -9,37 +9,35 @@ import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import ua.com.gup.config.filter.OAuthFilter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @ComponentScan(value = "ua.com.gup.api.oauth2")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public FilterRegistrationBean characterEncodingFilterRegistration() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new CharacterEncodingFilter("UTF-8", true));
-        registration.addUrlPatterns("/*");
-        registration.setName("CharacterEncodingFilter");
-        registration.setOrder(1);
-        return registration;
+        return registerFilter(new CharacterEncodingFilter("UTF-8", true), "CharacterEncodingFilter", 1, "/*");
     }
 
     @Bean
     public FilterRegistrationBean oauthFilterRegistration() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new OAuthFilter());
-        registration.addUrlPatterns("/*");
-        registration.setName("OAuthFilter");
-        registration.setOrder(2);
-        return registration;
+        return registerFilter(new OAuthFilter(), "OAuthFilter", 2, "/*");
     }
 
     @Bean
     public FilterRegistrationBean springSecurityFilterRegistration() {
+        return registerFilter(new DelegatingFilterProxy(), "springSecurityFilterChain", 3, "/*");
+    }
+
+
+    private FilterRegistrationBean registerFilter(Filter filter, String name, int order, String... urlPatterns) {
         FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new DelegatingFilterProxy());
-        registration.addUrlPatterns("/*");
-        registration.setName("springSecurityFilterChain");
-        registration.setOrder(3);
+        registration.setFilter(filter);
+        registration.addUrlPatterns(urlPatterns);
+        registration.setName(name);
+        registration.setOrder(order);
         return registration;
+
     }
 }
