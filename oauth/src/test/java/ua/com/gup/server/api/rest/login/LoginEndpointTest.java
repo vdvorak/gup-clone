becc512723b416b2b657416c383d621d86f03251
ivalex <al.ivashkov@gmail.com>
2017-11-16 16:03:18 +0200

    commented test

--- oauth/src/test/java/ua/com/gup/server/api/rest/login/LoginEndpointTest.java
+++ oauth/src/test/java/ua/com/gup/server/api/rest/login/LoginEndpointTest.java
@@ -1,24 +1,13 @@
 package ua.com.gup.server.api.rest.login;
 
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import ua.com.gup.api.oauth2.LoginEndpoint;
 import ua.com.gup.server.config.TestConfig;
-//import ua.com.gup.service.profile.ProfilesService;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import ua.com.gup.service.profile.ProfilesService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {TestConfig.class})
