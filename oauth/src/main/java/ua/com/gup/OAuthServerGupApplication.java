99d75b603917b75436a6eb98fd9a6d500c640a6a
ivalex <al.ivashkov@gmail.com>
2017-11-21 17:41:49 +0200

    basic oauth security

--- oauth/src/main/java/ua/com/gup/OAuthServerGupApplication.java
+++ oauth/src/main/java/ua/com/gup/OAuthServerGupApplication.java
@@ -2,21 +2,23 @@ package ua.com.gup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import ua.com.gup.config.*;
 
 @EnableAutoConfiguration
 @EnableResourceServer
-public class OAuthGupApplication {
+//@EnableZuulProxy
+public class OAuthServerGupApplication {
     public static void main(String[] args) {
         SpringApplication.run(new Object[]{WebConfig.class
                 , RootConfig.class
-                , SecurityConfig.class
+                , WebSecurityConfig.class
                 , SwaggerConfig.class
                 , MailConfig.class
                 , MongoConfig.class
-                , AuthServerConfig.class
-                , OAuthGupApplication.class
+                , OAuth2AuthorizationServerConfig.class
+                , OAuthServerGupApplication.class
         }, args);
     }
 }
