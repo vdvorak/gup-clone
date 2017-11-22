2f2cec672cb6aca33d75e5793ca7828cdb3978ad
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-11-22 14:18:29 +0200

    re-factoring auto import

--- rent-ui/src/main/java/ua/com/gup/rent/config/RentRootConfig.java
+++ rent-ui/src/main/java/ua/com/gup/rent/config/RentRootConfig.java
@@ -3,8 +3,8 @@ package ua.com.gup.rent.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import ua.com.gup.rent.mapper.RentObjectMapper;
-import ua.com.gup.rent.service.GenericService;
-import ua.com.gup.rent.validator.CategoryDTOValidator;
+import ua.com.gup.rent.service.abstracted.generic.GenericService;
+import ua.com.gup.rent.validator.category.CategoryDTOValidator;
 
 @Configuration
 @ComponentScan(basePackageClasses = {GenericService.class, RentObjectMapper.class, CategoryDTOValidator.class})
