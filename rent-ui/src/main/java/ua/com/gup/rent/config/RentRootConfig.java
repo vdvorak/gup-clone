f79e102d63940045347a8c64afdccdb1096e29de
ivalex <al.ivashkov@gmail.com>
2017-11-22 18:51:16 +0200

    before merge

--- rent-ui/src/main/java/ua/com/gup/rent/config/RentRootConfig.java
+++ rent-ui/src/main/java/ua/com/gup/rent/config/RentRootConfig.java
@@ -2,11 +2,8 @@ package ua.com.gup.rent.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import ua.com.gup.rent.mapper.RentObjectMapper;
-import ua.com.gup.rent.service.GenericService;
-import ua.com.gup.rent.validator.CategoryDTOValidator;
 
 @Configuration
-@ComponentScan(basePackageClasses = {GenericService.class, RentObjectMapper.class, CategoryDTOValidator.class})
+@ComponentScan(basePackages = {"ua.com.gup.rent.service", "ua.com.gup.rent.mapper", "ua.com.gup.rent.validator"})
 public class RentRootConfig {
 }
