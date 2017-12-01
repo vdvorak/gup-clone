5390e8fede7c2b82919e8bd111e38d1d7fcfc594
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-12-01 17:39:46 +0200

    re-factoring class moving

--- rent-ui/src/main/java/ua/com/gup/rent/repository/rent/RentObjectRepository.java
+++ rent-ui/src/main/java/ua/com/gup/rent/repository/rent/RentObjectRepository.java
@@ -1,6 +1,8 @@
 package ua.com.gup.rent.repository.rent;
 
-public interface RentObjectRepository extends ua.com.gup.rent.repository.abstracted.generic.RentGenericRepository<ua.com.gup.rent.model.mongo.rent.RentObject, String> {
+import ua.com.gup.rent.model.mongo.rent.Rent;
+
+public interface RentObjectRepository extends ua.com.gup.rent.repository.abstracted.generic.RentGenericRepository<Rent, String> {
     void deleteById(String rentObjectId);
 
     boolean isOwner(String rentObjectId, String userId);
--- rent-ui/src/main/java/ua/com/gup/rent/repository/rent/impl/RentObjectRepositoryImpl.java
+++ rent-ui/src/main/java/ua/com/gup/rent/repository/rent/impl/RentObjectRepositoryImpl.java
@@ -3,34 +3,35 @@ package ua.com.gup.rent.repository.rent.impl;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
-import ua.com.gup.rent.model.mongo.rent.RentObject;
+import ua.com.gup.rent.model.mongo.rent.Rent;
+import ua.com.gup.rent.repository.abstracted.RentGenericRepositoryImpl;
 import ua.com.gup.rent.repository.rent.RentObjectRepository;
 
 import javax.annotation.PostConstruct;
 
 @Repository
-public class RentObjectRepositoryImpl extends ua.com.gup.rent.repository.abstracted.RentGenericRepositoryImpl<ua.com.gup.rent.model.mongo.rent.RentObject, String> implements RentObjectRepository {
+public class RentObjectRepositoryImpl extends RentGenericRepositoryImpl<Rent, String> implements RentObjectRepository {
 
     public RentObjectRepositoryImpl() {
-        super(RentObject.class);
+        super(Rent.class);
     }
 
     @PostConstruct
     void init() {
-        if (!mongoTemplate.collectionExists(RentObject.class)) {
-            mongoTemplate.createCollection(RentObject.class);
+        if (!mongoTemplate.collectionExists(Rent.class)) {
+            mongoTemplate.createCollection(Rent.class);
         }
     }
 
     @Override
     public void deleteById(String rentObjectId) {
         Query deleteQuery = new Query(Criteria.where("id").is(rentObjectId));
-        mongoTemplate.remove(deleteQuery, RentObject.class);
+        mongoTemplate.remove(deleteQuery, Rent.class);
     }
 
     @Override
     public boolean isOwner(String rentObjectId, String userId) {
         Query exists = new Query(Criteria.where("id").is(rentObjectId).and("ownerId").is(userId));
-        return mongoTemplate.exists(exists, RentObject.class);
+        return mongoTemplate.exists(exists, Rent.class);
     }
 }
