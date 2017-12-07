d85eb9aaa08f143fb574cf11db644c942bb48236
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-12-07 18:46:56 +0200

    add save Rent Offer method create SeoUrl

---
+++ rent-ui/src/main/java/ua/com/gup/rent/repository/rent/offer/RentOfferRepositoryCRUD.java
@@ -0,0 +1,26 @@
+package ua.com.gup.rent.repository.rent.offer;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import ua.com.gup.rent.model.enumeration.RentOfferStatus;
+import ua.com.gup.rent.model.mongo.rent.RentOffer;
+
+import java.util.Optional;
+
+/**
+ * Repository for the offer entity.
+ */
+public interface RentOfferRepositoryCRUD extends MongoRepository<RentOffer, String> {
+
+    Optional<RentOffer> findOneBySeoUrl(String seoUrl);
+
+    Page<RentOffer> findAllByStatusAndAuthorId(RentOfferStatus status, String authorId, Pageable pageable);
+
+    Page<RentOffer> findAllByStatus(RentOfferStatus status, Pageable pageable);
+
+    Optional<RentOffer> findOfferByIdAndAuthorId(String offerId, String authorId);
+    
+    Boolean existsByIdAndStatus(String id, RentOfferStatus status);
+
+}
