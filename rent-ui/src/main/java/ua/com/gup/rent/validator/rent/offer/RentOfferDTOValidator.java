5ea0515b801bba61e84407f4f385c661a8148b4c
Victor Dvorak <viktor.dvorak83@gmail.com>
2017-12-14 16:28:22 +0200

    hot fix and add field to Rent Offer

--- rent-ui/src/main/java/ua/com/gup/rent/validator/rent/offer/RentOfferDTOValidator.java
+++ rent-ui/src/main/java/ua/com/gup/rent/validator/rent/offer/RentOfferDTOValidator.java
@@ -114,102 +114,110 @@ public class RentOfferDTOValidator implements Validator {
                         (rentOfferCreateDTO.getAttrs() == null ? 0 : rentOfferCreateDTO.getAttrs().size()) +
                         (rentOfferCreateDTO.getAttrs() == null ? 0 : rentOfferCreateDTO.getAttrs().size());
                 if (!isRentOfferUpdateDTO || (isRentOfferUpdateDTO && numberOfAllAttrs > 0)) {
-                    final SortedSet<RentOfferCategoryAttributeDTO> categoryAttributeDTOS = rentOfferCategoryAttributeService.findAllCategoryAttributeDTO().get(rentOfferCreateDTO.getCategory());
-                    final Map<String, RentOfferCategoryAttributeDTO> categoryAttributeDTOMap = categoryAttributeDTOS.stream().collect(Collectors.toMap(RentOfferCategoryAttributeDTO::getKey, Function.identity()));
-                    // checking values of attrs
-                    if (rentOfferCreateDTO.getAttrs() != null) {
-                        for (String key : rentOfferCreateDTO.getAttrs().keySet()) {
-                            final RentOfferCategoryAttributeDTO categoryAttributeDTO = categoryAttributeDTOMap.get(key);
-                            if (categoryAttributeDTO == null) {
-                                errors.rejectValue("attrs", "attrs." + key + ".unknown", null, "Unknown attribute");
-                            } else {
-                                RentOfferCategoryAttributeValueDTO attributeValueDTO = new RentOfferCategoryAttributeValueDTO();
-                                attributeValueDTO.setKey(rentOfferCreateDTO.getAttrs().get(key));
-                                final Set<RentOfferCategoryAttributeValueDTO> valuesToCheck = new HashSet<>(categoryAttributeDTO.getValues()); // TreeSet to HashSet
-                                if (!valuesToCheck.contains(attributeValueDTO)) { //
-                                    errors.rejectValue("attrs", "attrs." + key + ".value.unknown", null, "Unknown value <" + rentOfferCreateDTO.getAttrs().get(key) + "> for attr <" + key + ">");
-                                }
-                            }
-                        }
-                    }
-                    // checking values of multiAttrs
-                    if (rentOfferCreateDTO.getMultiAttrs() != null) {
-                        for (String key : rentOfferCreateDTO.getMultiAttrs().keySet()) {
-                            final RentOfferCategoryAttributeDTO categoryAttributeDTO = categoryAttributeDTOMap.get(key);
-                            if (categoryAttributeDTO == null) {
-                                errors.rejectValue("multiAttrs", "multiAttrs." + key + ".unknown", null, "Unknown attribute");
-                            } else {
-                                final String[] values = rentOfferCreateDTO.getMultiAttrs().get(key).split(",");
-                                for (String value : values) {
-                                    RentOfferCategoryAttributeValueDTO attributeValueDTO = new RentOfferCategoryAttributeValueDTO();
-                                    attributeValueDTO.setKey(value);
-                                    final Set<RentOfferCategoryAttributeValueDTO> valuesToCheck = new HashSet<>(categoryAttributeDTO.getValues());
-                                    if (!valuesToCheck.contains(attributeValueDTO)) { //
-                                        errors.rejectValue("multiAttrs", "multiAttrs." + key + ".value.unknown", null, "Unknown value <" + value + "> for attr <" + key + ">");
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    // checking values of numAttrs
-                    if (rentOfferCreateDTO.getNumAttrs() != null) {
-                        for (String key : rentOfferCreateDTO.getNumAttrs().keySet()) {
-                            final RentOfferCategoryAttributeDTO categoryAttributeDTO = categoryAttributeDTOMap.get(key);
-                            if (categoryAttributeDTO == null) {
-                                errors.rejectValue("numAttrs", "numAttrs." + key + ".unknown", null, "Unknown attribute");
-                            } else {
-                                final RentOfferCategoryAttributeValidatorDTO validator = categoryAttributeDTO.getValidator();
-                                final BigDecimal value = rentOfferCreateDTO.getNumAttrs().get(key);
-                                if (validator.getMin() != null) {
-                                    if (validator.getMin().compareTo(value) > 0) {
-                                        errors.rejectValue("numAttrs", "numAttrs." + key + ".min", null, "The value = " + value + " is less then min value = " + validator.getMin());
-                                    }
-                                }
-                                if (validator.getMax() != null) {
-                                    if (validator.getMax().compareTo(value) < 0) {
-                                        errors.rejectValue("numAttrs", "numAttrs." + key + ".max", null, "The value = " + value + " is more then max value = " + validator.getMax());
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    // checking values of boolAttrs
-                    if (rentOfferCreateDTO.getBoolAttrs() != null) {
-                        for (String key : rentOfferCreateDTO.getBoolAttrs().keySet()) {
-                            final RentOfferCategoryAttributeDTO categoryAttributeDTO = categoryAttributeDTOMap.get(key);
-                            if (categoryAttributeDTO == null) {
-                                errors.rejectValue("boolAttrs", "boolAttrs." + key + ".unknown", null, "Unknown attribute");
-                            }
-                        }
-                    }
 
-                    // checking required attributes
-                    if (categoryAttributeDTOS != null) {
-                        for (RentOfferCategoryAttributeDTO categoryAttributeDTO : categoryAttributeDTOS) {
-                            if (categoryAttributeDTO.getType() == CommonCategoryAttributeType.SELECT) {
-                                if (rentOfferCreateDTO.getAttrs() == null || rentOfferCreateDTO.getAttrs().get(categoryAttributeDTO.getKey()) == null && categoryAttributeDTO.getValidator().isRequired()) {
-                                    errors.rejectValue("attrs", "attrs." + categoryAttributeDTO.getKey() + ".required", null, "Attribute is required");
-                                }
-                            } else if (categoryAttributeDTO.getType() == CommonCategoryAttributeType.MULTI_SELECT) {
-                                if (rentOfferCreateDTO.getMultiAttrs() == null || rentOfferCreateDTO.getMultiAttrs().get(categoryAttributeDTO.getKey()) == null && categoryAttributeDTO.getValidator().isRequired()) {
-                                    errors.rejectValue("multiAttrs", "multiAttrs." + categoryAttributeDTO.getKey() + ".required", null, "Attribute is required");
-                                }
-                            } else if (categoryAttributeDTO.getType() == CommonCategoryAttributeType.NUMBER) {
-                                if (rentOfferCreateDTO.getNumAttrs() == null || rentOfferCreateDTO.getNumAttrs().get(categoryAttributeDTO.getKey()) == null && categoryAttributeDTO.getValidator().isRequired()) {
-                                    errors.rejectValue("numAttrs", "numAttrs." + categoryAttributeDTO.getKey() + ".required", null, "Attribute is required");
-                                }
-                            } else if (categoryAttributeDTO.getType() == CommonCategoryAttributeType.BOOLEAN) {
-                                if (rentOfferCreateDTO.getBoolAttrs() == null || rentOfferCreateDTO.getBoolAttrs().get(categoryAttributeDTO.getKey()) == null && categoryAttributeDTO.getValidator().isRequired()) {
-                                    errors.rejectValue("boolAttrs", "boolAttrs." + categoryAttributeDTO.getKey() + ".required", null, "Attribute is required");
+                    Map<String, RentOfferCategoryAttributeDTO> categoryAttributeDTOMap = null;
+
+                    try {
+                        final SortedSet<RentOfferCategoryAttributeDTO> categoryAttributeDTOS = rentOfferCategoryAttributeService.findAllCategoryAttributeDTO().get(rentOfferCreateDTO.getCategory());
+                        //check category attribute by category
+                        categoryAttributeDTOMap = categoryAttributeDTOS.stream().collect(Collectors.toMap(RentOfferCategoryAttributeDTO::getKey, Function.identity()));
+                        // checking required attributes
+                        if (categoryAttributeDTOS != null) {
+                            for (RentOfferCategoryAttributeDTO categoryAttributeDTO : categoryAttributeDTOS) {
+                                if (categoryAttributeDTO.getType() == CommonCategoryAttributeType.SELECT) {
+                                    if (rentOfferCreateDTO.getAttrs() == null || rentOfferCreateDTO.getAttrs().get(categoryAttributeDTO.getKey()) == null && categoryAttributeDTO.getValidator().isRequired()) {
+                                        errors.rejectValue("attrs", "attrs." + categoryAttributeDTO.getKey() + ".required", null, "Attribute is required");
+                                    }
+                                } else if (categoryAttributeDTO.getType() == CommonCategoryAttributeType.MULTI_SELECT) {
+                                    if (rentOfferCreateDTO.getMultiAttrs() == null || rentOfferCreateDTO.getMultiAttrs().get(categoryAttributeDTO.getKey()) == null && categoryAttributeDTO.getValidator().isRequired()) {
+                                        errors.rejectValue("multiAttrs", "multiAttrs." + categoryAttributeDTO.getKey() + ".required", null, "Attribute is required");
+                                    }
+                                } else if (categoryAttributeDTO.getType() == CommonCategoryAttributeType.NUMBER) {
+                                    if (rentOfferCreateDTO.getNumAttrs() == null || rentOfferCreateDTO.getNumAttrs().get(categoryAttributeDTO.getKey()) == null && categoryAttributeDTO.getValidator().isRequired()) {
+                                        errors.rejectValue("numAttrs", "numAttrs." + categoryAttributeDTO.getKey() + ".required", null, "Attribute is required");
+                                    }
+                                } else if (categoryAttributeDTO.getType() == CommonCategoryAttributeType.BOOLEAN) {
+                                    if (rentOfferCreateDTO.getBoolAttrs() == null || rentOfferCreateDTO.getBoolAttrs().get(categoryAttributeDTO.getKey()) == null && categoryAttributeDTO.getValidator().isRequired()) {
+                                        errors.rejectValue("boolAttrs", "boolAttrs." + categoryAttributeDTO.getKey() + ".required", null, "Attribute is required");
+                                    }
                                 }
                             }
                         }
+                    } catch (NullPointerException ex) {
+                        errors.rejectValue("category", "rent.offer.category." + rentOfferCreateDTO.getCategory() + ".unknown", null, "This Category not set Category Attribute please check");
+                    }
+                    // checking values of attrs
+                    if (categoryAttributeDTOMap != null) {
+                        if (rentOfferCreateDTO.getAttrs() != null) {
+                            for (String key : rentOfferCreateDTO.getAttrs().keySet()) {
+                                final RentOfferCategoryAttributeDTO categoryAttributeDTO = categoryAttributeDTOMap.get(key);
+                                if (categoryAttributeDTO == null) {
+                                    errors.rejectValue("attrs", "attrs." + key + ".unknown", null, "Unknown attribute");
+                                } else {
+                                    RentOfferCategoryAttributeValueDTO attributeValueDTO = new RentOfferCategoryAttributeValueDTO();
+                                    attributeValueDTO.setKey(rentOfferCreateDTO.getAttrs().get(key));
+                                    final Set<RentOfferCategoryAttributeValueDTO> valuesToCheck = new HashSet<>(categoryAttributeDTO.getValues()); // TreeSet to HashSet
+                                    if (!valuesToCheck.contains(attributeValueDTO)) { //
+                                        errors.rejectValue("attrs", "attrs." + key + ".value.unknown", null, "Unknown value <" + rentOfferCreateDTO.getAttrs().get(key) + "> for attr <" + key + ">");
+                                    }
+                                }
+                            }
+                        }
+                        // checking values of multiAttrs
+                        if (rentOfferCreateDTO.getMultiAttrs() != null) {
+                            for (String key : rentOfferCreateDTO.getMultiAttrs().keySet()) {
+                                final RentOfferCategoryAttributeDTO categoryAttributeDTO = categoryAttributeDTOMap.get(key);
+                                if (categoryAttributeDTO == null) {
+                                    errors.rejectValue("multiAttrs", "multiAttrs." + key + ".unknown", null, "Unknown attribute");
+                                } else {
+                                    final String[] values = rentOfferCreateDTO.getMultiAttrs().get(key).split(",");
+                                    for (String value : values) {
+                                        RentOfferCategoryAttributeValueDTO attributeValueDTO = new RentOfferCategoryAttributeValueDTO();
+                                        attributeValueDTO.setKey(value);
+                                        final Set<RentOfferCategoryAttributeValueDTO> valuesToCheck = new HashSet<>(categoryAttributeDTO.getValues());
+                                        if (!valuesToCheck.contains(attributeValueDTO)) { //
+                                            errors.rejectValue("multiAttrs", "multiAttrs." + key + ".value.unknown", null, "Unknown value <" + value + "> for attr <" + key + ">");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        // checking values of numAttrs
+                        if (rentOfferCreateDTO.getNumAttrs() != null) {
+                            for (String key : rentOfferCreateDTO.getNumAttrs().keySet()) {
+                                final RentOfferCategoryAttributeDTO categoryAttributeDTO = categoryAttributeDTOMap.get(key);
+                                if (categoryAttributeDTO == null) {
+                                    errors.rejectValue("numAttrs", "numAttrs." + key + ".unknown", null, "Unknown attribute");
+                                } else {
+                                    final RentOfferCategoryAttributeValidatorDTO validator = categoryAttributeDTO.getValidator();
+                                    final BigDecimal value = rentOfferCreateDTO.getNumAttrs().get(key);
+                                    if (validator.getMin() != null) {
+                                        if (validator.getMin().compareTo(value) > 0) {
+                                            errors.rejectValue("numAttrs", "numAttrs." + key + ".min", null, "The value = " + value + " is less then min value = " + validator.getMin());
+                                        }
+                                    }
+                                    if (validator.getMax() != null) {
+                                        if (validator.getMax().compareTo(value) < 0) {
+                                            errors.rejectValue("numAttrs", "numAttrs." + key + ".max", null, "The value = " + value + " is more then max value = " + validator.getMax());
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        // checking values of boolAttrs
+                        if (rentOfferCreateDTO.getBoolAttrs() != null) {
+                            for (String key : rentOfferCreateDTO.getBoolAttrs().keySet()) {
+                                final RentOfferCategoryAttributeDTO categoryAttributeDTO = categoryAttributeDTOMap.get(key);
+                                if (categoryAttributeDTO == null) {
+                                    errors.rejectValue("boolAttrs", "boolAttrs." + key + ".unknown", null, "Unknown attribute");
+                                }
+                            }
+                        }
+
                     }
                 }
             }
         }
-
     }
-
 }
 
