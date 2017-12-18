32730005a1b9ecdd1da1b8bec4e680cc7c3ae5f2
Dmitriy Terlyha <terlyha.d@gmail.com>
2017-12-18 14:32:58 +0200

    refactoring CommonOfferServiceImpl - abstract UI and Rest - impl

---
+++ rent-ui/src/main/java/ua/com/gup/rent/service/rent/impl/CommonOfferServiceRentImpl.java
@@ -0,0 +1,8 @@
+package ua.com.gup.rent.service.rent.impl;
+
+import org.springframework.stereotype.Service;
+import ua.com.gup.common.service.impl.CommonOfferServiceImpl;
+
+@Service
+public class CommonOfferServiceRentImpl extends CommonOfferServiceImpl {
+}
