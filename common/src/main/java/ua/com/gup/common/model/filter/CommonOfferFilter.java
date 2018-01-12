eb94e47df6c351f110e6038e2c25b0c3ebcb28be
ivalex <al.ivashkov@gmail.com>
2018-01-12 15:04:39 +0200

    added filter by one "category" to common (rent) offer filter

--- common/src/main/java/ua/com/gup/common/model/filter/CommonOfferFilter.java
+++ common/src/main/java/ua/com/gup/common/model/filter/CommonOfferFilter.java
@@ -1,14 +1,21 @@
 package ua.com.gup.common.model.filter;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+@Data
 @ApiModel(description = "Offer filter model")
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public abstract class CommonOfferFilter implements Serializable {
 
     private static final long serialVersionUID = 1232825578694716871L;
@@ -18,9 +25,6 @@ public abstract class CommonOfferFilter implements Serializable {
 
     @ApiModelProperty(value = "Field form object Profile", position = 5)
     private CommonAuthorFilter authorFilter;
-
-    @ApiModelProperty(value = "Categories with ',' delimiter", position = 10)
-    private Integer[] categories;
 
     @ApiModelProperty(value = "Filter for address", position = 30)
     private CommonAddressFilter address;
@@ -46,111 +50,8 @@ public abstract class CommonOfferFilter implements Serializable {
     @ApiModelProperty(value = "Seo url's with ',' delimiter", position = 80)
     private String[] seoUrls;
 
-    public String getQuery() {
-        return query;
-    }
-
-    public void setQuery(String query) {
-        this.query = query;
-    }
-
-    public Integer[] getCategories() {
-        return categories;
-    }
-
-    public void setCategories(Integer[] categories) {
-        this.categories = categories;
-    }
-
-
-
-    public CommonAddressFilter getAddress() {
-        return address;
-    }
-
-    public void setAddress(CommonAddressFilter address) {
-        this.address = address;
-    }
-
-    public MoneyFilter getPrice() {
-        return price;
-    }
-
-    public void setPrice(MoneyFilter price) {
-        this.price = price;
-    }
-
-    public List<CommonAttributeFilter> getAttrs() {
-        return attrs;
-    }
-
-    public void setAttrs(List<CommonAttributeFilter> attrs) {
-        this.attrs = attrs;
-    }
-
-    public List<CommonAttributeFilter> getMultiAttrs() {
-        return multiAttrs;
-    }
-
-    public void setMultiAttrs(List<CommonAttributeFilter> multiAttrs) {
-        this.multiAttrs = multiAttrs;
-    }
-
-    public List<NumericAttributeFilter> getNumAttrs() {
-        return numAttrs;
-    }
-
-    public void setNumAttrs(List<NumericAttributeFilter> numAttrs) {
-        this.numAttrs = numAttrs;
-    }
-
-    public List<BooleanAttributeFilter> getBoolAttrs() {
-        return boolAttrs;
-    }
-
-    public void setBoolAttrs(List<BooleanAttributeFilter> boolAttrs) {
-        this.boolAttrs = boolAttrs;
-    }
-
-    public CommonAuthorFilter getAuthorFilter() {
-        return authorFilter;
-    }
-
-    public void setAuthorFilter(CommonAuthorFilter authorFilter) {
-        this.authorFilter = authorFilter;
-    }
-
-
-    public String[] getSeoUrls() {
-        return seoUrls;
-    }
-
-    public void setSeoUrls(String[] seoUrls) {
-        this.seoUrls = seoUrls;
-    }
-
-    public CommonCoordinatesFilter getCoordinates() {
-        return coordinates;
-    }
-
-    public void setCoordinates(CommonCoordinatesFilter coordinates) {
-        this.coordinates = coordinates;
-
-    }
-
-    @Override
-    public String toString() {
-        return "OfferFilter{" +
-                "query='" + query + '\'' +
-                ", authorFilter=" + authorFilter +
-                ", categories=" + Arrays.toString(categories) +
-                ", address=" + address +
-                ", price=" + price +
-                ", attrs=" + attrs +
-                ", multiAttrs=" + multiAttrs +
-                ", numAttrs=" + numAttrs +
-                ", boolAttrs=" + boolAttrs +
-                '}';
-    }
+    @ApiModelProperty("Date from inclusive")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate createdDate;
 
 }
--- rent-ui/src/main/java/ua/com/gup/rent/filter/RentOfferFilter.java
+++ rent-ui/src/main/java/ua/com/gup/rent/filter/RentOfferFilter.java
@@ -3,11 +3,16 @@ package ua.com.gup.rent.filter;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import ua.com.gup.common.model.filter.*;
+import lombok.Getter;
+import lombok.Setter;
+import ua.com.gup.common.model.filter.CommonOfferFilter;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
+@ApiModel(description = "Rent Offer filter model")
+@Getter
+@Setter
 public class RentOfferFilter extends CommonOfferFilter {
+
+    @ApiModelProperty(value = "Only one category (level not important)", position = 10)
+    private Integer category;
+
 }
