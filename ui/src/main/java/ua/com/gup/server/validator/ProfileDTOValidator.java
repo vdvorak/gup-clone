778d85c7e4abdaa2f8af14eb081b17b49ad3ee39
Victor Dvorak <viktor.dvorak83@gmail.com>
2018-01-23 19:23:58 +0200

    hot fix phoneNumber maybe null

--- ui/src/main/java/ua/com/gup/server/validator/ProfileDTOValidator.java
+++ ui/src/main/java/ua/com/gup/server/validator/ProfileDTOValidator.java
@@ -24,29 +24,17 @@ public class ProfileDTOValidator implements Validator {
 
         boolean isUpdateDTO = EditProfileDTO.class.isInstance(target);
         ProfileDTO profileDTO = (ProfileDTO) target;
-
-         //FOR update required
-        /*if (isUpdateDTO) {
-            ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id", "id.required");
-        }*/
-       /* if (!isUpdateDTO) {
-            ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "title.required");
-        }*/
-
+        
         if (profileDTO.getContact() != null) {
-
+         if (profileDTO.getContact().getContactPhones() != null ) {
             if (profileDTO.getContact().getContactPhones() == null || profileDTO.getContact().getContactPhones().size() == 0) {
-                errors.rejectValue("contact.contactPhones", "contact.contactPhones.size", null, "At least one phone number should be");
-            } else {
                 for (String phoneNo : profileDTO.getContact().getContactPhones()) {
                     if (phoneNo == null || !phoneNo.matches("^380[0-9]{9}$")) {
                         errors.rejectValue("contact.contactPhones", "contact.contactPhones.format", null, "Phone number format is ^380[0-9]{9}$");
                     }
                 }
-            }
+            } 
         }
-
-
     }
 }
 
