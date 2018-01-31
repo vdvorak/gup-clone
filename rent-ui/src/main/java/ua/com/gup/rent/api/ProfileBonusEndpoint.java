ed3bf59c67ab37185e5e79be71a308bdbb8b3c81
Victor Dvorak <viktor.dvorak83@gmail.com>
2018-01-31 19:22:20 +0200

    https://redmine.dev.gup.ua/redmine/issues/1834 add  ProfileBonusEndpoint  "/api/rest/profilesService/profile/bonus/" .

--- rent-ui/src/main/java/ua/com/gup/rent/api/ProfileBonusEndpoint.java
+++ rent-ui/src/main/java/ua/com/gup/rent/api/ProfileBonusEndpoint.java
@@ -11,7 +11,11 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import ua.com.gup.common.command.CommandException;
+import ua.com.gup.common.dto.CommonCreateDTO;
+import ua.com.gup.rent.command.rent.profile.bonus.CreateProfileBonusCommand;
+import ua.com.gup.rent.command.rent.profile.bonus.UpdateProfileBonusCommand;
 import ua.com.gup.rent.component.executor.RentCommandExecutor;
+import ua.com.gup.rent.mapper.ProfileBonusMapper;
 import ua.com.gup.rent.service.dto.profile.bonus.ProfileCreateBonusDTO;
 import ua.com.gup.rent.service.dto.profile.bonus.ProfileEditBonusDTO;
 import ua.com.gup.rent.service.profile.bonus.ProfileBonusService;
@@ -33,6 +37,8 @@ public class ProfileBonusEndpoint {
     @Autowired
     private ProfileBonusDTOValidator profileBonusDTOValidator;
 
+    private ProfileBonusMapper profileBonusMapper;
+
     @Autowired
     private RentCommandExecutor executor;
 
@@ -48,15 +54,12 @@ public class ProfileBonusEndpoint {
 
     @ApiOperation(value = "Get Bonus")
     @RequestMapping(value = "/get/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("isAuthenticated()")
     public ResponseEntity getBonus(@PathVariable(name = "name") String name) throws CommandException {
         ProfileEditBonusDTO profileBonusDTO = profileBonusService.findOneByName(name);
         if (profileBonusDTO == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        // UpdateRentOfferCommand updateRentOfferCommand = new UpdateRentOfferCommand(offerService, offerUpdateDTO, offerService.getRentOfferIdBySeoUrl(seoUrl));
-        //   executor.doCommand(updateRentOfferCommand);
-        return new ResponseEntity<>(profileBonusDTO,HttpStatus.OK);
+        return new ResponseEntity<>(profileBonusDTO, HttpStatus.OK);
     }
 
     @ApiOperation(value = "Create Bonus")
@@ -65,15 +68,14 @@ public class ProfileBonusEndpoint {
     @RequestMapping(value = "/create", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity createBonus(@Valid @RequestBody ProfileCreateBonusDTO createBonusDTO) throws CommandException {
         log.debug(" create bonus  : {}", createBonusDTO);
-        // CreateRentOfferCommand createRentOfferCommand = new CreateRentOfferCommand(offerService, offerCreateDTO);
-        // executor.doCommand(createRentOfferCommand);
-        //  return new ResponseEntity<>(new CommonCreateDTO(createRentOfferCommand.getObjectId()), HttpStatus.CREATED);
-         return new ResponseEntity<>(HttpStatus.CREATED);
+        CreateProfileBonusCommand createProfileBonusCommand = new CreateProfileBonusCommand(profileBonusService, (ProfileEditBonusDTO) createBonusDTO, profileBonusMapper);
+        executor.doCommand(createProfileBonusCommand);
+        return new ResponseEntity<>(new CommonCreateDTO(createProfileBonusCommand.getObjectId()), HttpStatus.CREATED);
     }
 
     @ApiOperation(value = "Edit Bonus")
-    @RequestMapping(value = "/edit/{name}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/edit/{name}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity editBonus(@PathVariable(name = "name") String name,
                                     @Valid @RequestBody ProfileEditBonusDTO editBonusDTO) throws CommandException {
         log.debug("Edit Bonus : {}", editBonusDTO);
@@ -81,11 +83,10 @@ public class ProfileBonusEndpoint {
         if (profileBonusDTO == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        // UpdateRentOfferCommand updateRentOfferCommand = new UpdateRentOfferCommand(offerService, offerUpdateDTO, offerService.getRentOfferIdBySeoUrl(seoUrl));
-        //   executor.doCommand(updateRentOfferCommand);
+        UpdateProfileBonusCommand updateProfileBonusCommand = new UpdateProfileBonusCommand(profileBonusService, editBonusDTO, profileBonusMapper);
+        executor.doCommand(updateProfileBonusCommand);
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
 
 }
 
